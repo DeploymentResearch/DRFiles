7ce40f6af720f322d54b8527769b5f77ad5be03b
arwidmark <arwidmark@gmail.com>
2017-03-23 10:37:17 -0500

    Updated Intune Script

--- Scripts/PrepMicrosoftIntune.ps1
+++ Scripts/PrepMicrosoftIntune.ps1
@@ -11,12 +11,41 @@
 # is not supported by the authors or Deployment Artist.
 #----------------------------------------------------------------------------
 
+# Initialize
+$ScriptDir = split-path -parent $MyInvocation.MyCommand.Path
+$ScriptName = split-path -leaf $MyInvocation.MyCommand.Path
+$Lang = (Get-Culture).Name
+$Architecture = $env:PROCESSOR_ARCHITECTURE
+$Logpath = "C:\Windows\Temp"
+$LogFile = $Logpath + "\" + "$ScriptName.txt"
+
+# Start logging
+Start-transcript -path $LogFile
+
+#Output base info
+Write-Output ""
+Write-Output "$ScriptName - ScriptDir: $ScriptDir"
+Write-Output "$ScriptName - ScriptName: $ScriptName"
+Write-Output "$ScriptName - Current Culture: $Lang"
+Write-Output "$ScriptName - Architecture: $Architecture"
+Write-Output "$ScriptName - Log: $LogFile"
+
 # Copy the Microsoft Intune Setup files locally
 Copy-Item .\Microsoft_Intune_Setup.exe "C:\Windows\Temp"
 Copy-Item .\MicrosoftIntune.accountcert "C:\Windows\Temp"
 
 # Create a registry key to specify that the Intune client installation is pending registration in the cloud
-Reg.exe add HKEY_LOCAL_MACHINE\Software\Microsoft\Onlinemanagement\Deployment /v WindowsIntuneEnrollPending /t REG_DWORD /d 1
+New-Item -Path 'HKLM:\Software\Microsoft\Onlinemanagement\Deployment' -Force
+New-ItemProperty -Path 'HKLM:\Software\Microsoft\Onlinemanagement\Deployment' -PropertyType dword -Name "WindowsIntuneEnrollPending" -Value 00000001 
 
 # Run the Installer locally with the argument /PrepareEnroll
-& C:\Windows\Temp\Windows_Intune_Setup.exe /PrepareEnroll
+$SetupName = "Microsoft Intune Client Software"
+$SetupFile = "C:\Windows\Temp\Microsoft_Intune_Setup.exe"
+$SetupSwitches = "/PrepareEnroll"
+Write-Output "Starting install of $SetupName"
+Write-Output "Command line to start is: $SetupFile $SetupSwitches"
+Start-Process -FilePath $SetupFile -ArgumentList $SetupSwitches -NoNewWindow -Wait
+Write-Output "Finished installing $SetupName"
+
+# Stop logging
+Stop-Transcript
