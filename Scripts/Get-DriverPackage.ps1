9fa1d3ac388f18bb717432328ee0aeeb156569d7
arwidmark <arwidmark@gmail.com>
2017-03-25 11:35:28 -0500

    Added scripts from Matthew Teegarden

---
+++ Scripts/Get-DriverPackage.ps1
@@ -0,0 +1,257 @@
+﻿# Name:         Get-DriverPacakge
+# Description:  Gets the driver package ID for download during a Task Sequence
+# Author:       Matthew Teegarden
+	
+try
+{	
+    Function Main
+	{
+		# Function Started
+		LogTraceMessage "*** Function Main Started ***"
+        Write-Verbose "*** Function Main Started ***"
+
+		# Set Global Environment Variables (Inputs)
+		SetGlobalEnvVariables
+
+		# Import PS Modules
+		ImportPsModules
+
+        # Create and add more funcitons
+
+        # Import Driver Package XML created from SCCM
+        ImportDiverPackageXML
+
+        # Get the OS Architecture
+        GetOSArchitecture
+
+        # Get the OS Name
+        GetOSVersionNumber
+
+        # Get the computer model
+        GetModel
+
+        # Get the driver package to download
+        GetDriverPackage
+
+		# Function Finished
+		LogTraceMessage "*** Function Main Finished ***"
+        Write-Verbose "*** Function Main Finished ***"
+	}
+
+	Function SetGlobalEnvVariables
+	{
+		# Function Started
+        LogTraceMessage "*** Function SetGlobalEnvVariables Started ***"
+        Write-Verbose "*** Function SetGlobalEnvVariables Started ***"
+
+        # Base script variables.  No modification should be necessary
+
+		# Set variables with global scope
+		$script:TraceState = ''
+		LogTraceMessage "Variable TraceState set to $script:TraceState"
+        Write-Verbose "Variable TraceState set to $script:TraceState"
+
+		$script:ErrorMessage = ''
+		LogTraceMessage "Variable ErrorMessage set to $script:ErrorMessage"
+        Write-Verbose "Variable ErrorMessage set to $script:ErrorMessage"
+
+		$script:ErrorState = 0
+		LogTraceMessage "Variable ErrorState set to $script:ErrorState"
+        Write-Verbose "Variable ErrorState set to $script:ErrorState"
+
+        #Script variables.  Modify as necessary
+
+        $Script:LoggingPath = 'c:\Windows\Temp\Get-DriverPackages.log'
+        LogTraceMessage "Variable LoggingPath set to $script:LoggingPath"
+        Write-Verbose "Variable LoggingPath set to $script:LoggingPath"
+
+        # Add variables here
+
+        $Script:DefaultDriverPacakge = 'TEE00026'
+        LogTraceMessage "Variable DefaultDriverPacakge set to $script:DefaultDriverPacakge"
+        Write-Verbose "Variable DefaultDriverPacakge set to $script:DefaultDriverPacakge"
+
+ 	    # Function Finished
+		LogTraceMessage "*** Function SetGlobalEnvVariables Finished ***"
+        Write-Verbose "*** Function SetGlobalEnvVariables Finished ***"
+	}
+
+	Function ImportPsModules
+	{
+		# Function Started
+        LogTraceMessage "*** Function ImportPsModules Started ***"
+        Write-Verbose "*** Function ImportPsModules Started ***"
+
+        $script:tsenv = New-Object -COMObject Microsoft.SMS.TSEnvironment 
+
+      	# Function Finished
+		LogTraceMessage "*** Function ImportPsModules Finished ***"
+        Write-Verbose "*** Function ImportPsModules Finished ***"
+	}
+
+    Function ImportDiverPackageXML
+    {
+    	# Function Started
+        LogTraceMessage "*** Function ImportDiverPackageXML Started ***"
+        Write-Verbose "*** Function ImportDiverPackageXML Started ***"
+
+        $script:Packages = Import-Clixml .\driverpackages.xml
+
+      	# Function Finished
+		LogTraceMessage "*** Function ImportDiverPackageXML Finished ***"
+        Write-Verbose "*** Function ImportDiverPackageXML Finished ***"
+    }
+
+    Function GetOSArchitecture
+    {
+    	# Function Started
+        LogTraceMessage "*** Function GetOSArchitectureL Started ***"
+        Write-Verbose "*** Function GetOSArchitecture Started ***"
+
+        $script:OSArchitecture = $script:tsenv.Value("OSArchitecture")
+        
+        LogTraceMessage "OSArchitecture = $script:OSArchitecture"
+        Write-Verbose "OSArchitecture = $script:OSArchitecture"
+
+      	# Function Finished
+		LogTraceMessage "*** Function GetOSArchitecture Finished ***"
+        Write-Verbose "*** Function GetOSArchitecture Finished ***"
+    }
+
+    Function GetOSVersionNumber
+    {
+    	# Function Started
+        LogTraceMessage "*** Function GetOSName Started ***"
+        Write-Verbose "*** Function GetOSName Started ***"
+
+        $script:OSVersionNumber = $script:tsenv.Value("OSVersionNumber")
+        
+        LogTraceMessage "OSVersionNumber = $script:OSVersionNumber"
+        Write-Verbose "OSVersionNumber = $script:SVersionNumber"
+
+        If ($script:OSVersionNumber -like "10.*")
+        {
+            $script:OSVersionNumber = "Win10"
+        }
+        If ($script:OSVersionNumber -like "6.1*")
+        {
+            $script:OSVersionNumber = "Win7"
+        }
+
+        LogTraceMessage "OSVersionNumber set to $script:OSVersionNumber"
+        Write-Verbose "OSVersionNumber set to $script:OSVersionNumber"
+
+      	# Function Finished
+		LogTraceMessage "*** Function GetOSName Finished ***"
+        Write-Verbose "*** Function GetOSName Finished ***"
+    }
+
+    Function GetModel
+    {
+    	# Function Started
+        LogTraceMessage "*** Function ModelStarted ***"
+        Write-Verbose "*** Function Model Started ***"
+
+        $script:Model = (Get-CimInstance -ClassName win32_computersystemproduct -Namespace root\cimv2).Name 
+
+        LogTraceMessage "Model = $script:Model"
+        Write-Verbose "Model = $script:Model"
+
+      	# Function Finished
+		LogTraceMessage "*** Function Model Finished ***"
+        Write-Verbose "*** Function Model Finished ***"
+    }
+
+    Function GetDriverPackage
+    {
+    	# Function Started
+        LogTraceMessage "*** Function GetDriverPackage ***"
+        Write-Verbose "*** Function GetDriverPackage Started ***"
+
+        Foreach ($i in $script:Packages)
+        {
+            If ($i.name -like "*" + $script:model + "*" -and $i.name -like "*" + $script:OSVersionNumber + "*" -and $i.name -like "*" + $script:OSArchitecture + "*")
+            {
+                $script:tsenv.Value('OSDDownloadDownloadPackages') = $i.PackageID
+                $script:TSOSDDownloadDownloadPackages =  $script:tsenv.Value('OSDDownloadDownloadPackages')
+                LogTraceMessage "TSOSDDownloadDownloadPackages = $script:TSOSDDownloadDownloadPackages"
+                Write-Verbose "TSOSDDownloadDownloadPackages $script:TSOSDDownloadDownloadPackages"
+                
+                $script:tsenv.Value("DriverPackageToDownload") = $i.PackageID
+                $script:TSDefaultDriverPacakge = $script:tsenv.Value("DriverPackageToDownload")
+                LogTraceMessage "TSDefaultDriverPacakge changed to $script:TSDefaultDriverPacakge"
+                Write-Verbose "TSDefaultDriverPacakge changed to  $script:TSDefaultDriverPacakge"
+
+                $Script:DefaultDriverPacakge = $i.PackageID
+            }
+        }
+
+        If ($Script:DefaultDriverPacakge -eq 'TEE00026')
+        {
+            $script:tsenv.Value("DriverPackageToDownload") = $Script:DefaultDriverPacakge
+            $script:TSDefaultDriverPacakge = $script:tsenv.Value("DriverPackageToDownload")
+            LogTraceMessage "Default driver package to download = $script:TSDefaultDriverPacakge"
+            Write-Verbose "Default driver package to download = $script:TSDefaultDriverPacakge"
+        }
+  
+        
+      	# Function Finished
+		LogTraceMessage "*** Function GetDriverPackage Finished ***"
+        Write-Verbose "*** Function GetDriverPackage Finished ***"
+    }
+
+ 	Function LogTraceMessage ($strMessage)
+	{
+		[array]$script:TraceMessage += (Get-Date).ToString() + ':  ' + $strMessage + '~~' 
+    }
+    
+ 	# Script Started
+	LogTraceMessage "*** Get-DriverPacakge ***"
+    Write-Verbose "*** Get-DriverPacakge ***"
+
+	#Main
+	Main
+	
+}
+
+Catch
+{
+	# Catch Started
+	LogTraceMessage "*** Catch Started ***"
+    Write-Verbose "*** Catch Started ***"
+	
+	# Log error messages
+	$script:ErrorMessage = $Error[0].Exception.ToString()
+	LogTraceMessage "Variable ErrorMessage set to $script:ErrorMessage"
+    Write-Verbose "Variable ErrorMessage set to $script:ErrorMessage"
+	
+	$script:ErrorState = 3
+	LogTraceMessage "Variable ErrorState set to $script:ErrorState"
+    Write-Verbose "Variable ErrorState set to $script:ErrorState"
+	
+	# Catch Finished
+	LogTraceMessage "*** Catch Finished ***"
+    Write-Verbose "*** Catch Finished ***"
+}
+
+Finally
+{
+	# Finally Started
+	LogTraceMessage "*** Finally Started ***"
+    Write-Verbose "*** Finally Started ***" 
+
+	# Log Error State/Message
+	LogTraceMessage "Variable ErrorState = $script:ErrorState"
+    Write-Verbose  "Variable ErrorState = $script:ErrorState"
+	
+	# Finally Finished
+	LogTraceMessage "*** Finally Finished ***"
+	Write-Verbose "*** Finally Finished ***"
+	# Script Finished
+	LogTraceMessage "*** Get-DriverPacakge Finished ***"
+    Write-Verbose "*** Get-DriverPacakgefinished Finished ***"
+
+    # Write to log file
+    $script:TraceMessage | Out-File $script:LoggingPath
+
+}
---
+++ Scripts/Set-WMIEventingForSet-DriverPackageXML.ps1
@@ -0,0 +1,204 @@
+# Name:         Get-DriverPacakge
+# Description:  Gets the driver package ID for download during a Task Sequence
+# Author:       Matthew Teegarden
+	
+try
+{	
+    Function Main
+	{
+		# Function Started
+		LogTraceMessage "*** Function Main Started ***"
+        Write-Verbose "*** Function Main Started ***"
+
+		# Set Global Environment Variables (Inputs)
+		SetGlobalEnvVariables
+
+		# Import PS Modules
+		ImportPsModules
+
+        # Create and add more funcitons
+
+        # Create WMI EventFilter
+        WMI-InstanceFilter
+
+        # Create WMI InstanceConsumer
+        WMI-InstanceConsumer
+
+        # Create WMI FilterToConsumerBinding
+        WMI-FilterToConsumerBinding
+
+		# Function Finished
+		LogTraceMessage "*** Function Main Finished ***"
+        Write-Verbose "*** Function Main Finished ***"
+	}
+
+	Function SetGlobalEnvVariables
+	{
+		# Function Started
+        LogTraceMessage "*** Function SetGlobalEnvVariables Started ***"
+        Write-Verbose "*** Function SetGlobalEnvVariables Started ***"
+
+        # Base script variables.  No modification should be necessary
+
+		# Set variables with global scope
+		$script:TraceState = ''
+		LogTraceMessage "Variable TraceState set to $script:TraceState"
+        Write-Verbose "Variable TraceState set to $script:TraceState"
+
+		$script:ErrorMessage = ''
+		LogTraceMessage "Variable ErrorMessage set to $script:ErrorMessage"
+        Write-Verbose "Variable ErrorMessage set to $script:ErrorMessage"
+
+		$script:ErrorState = 0
+		LogTraceMessage "Variable ErrorState set to $script:ErrorState"
+        Write-Verbose "Variable ErrorState set to $script:ErrorState"
+
+        #Script variables.  Modify as necessary
+
+        $Script:LoggingPath = 'c:\Windows\Temp\Set-WMIEventingForSet-DriverPackageXML.log'
+        LogTraceMessage "Variable LoggingPath set to $script:LoggingPath"
+        Write-Verbose "Variable LoggingPath set to $script:LoggingPath"
+
+        # Add variables here
+
+        $Script:SiteServer = "TEE-CM1.EmptyGarden.btls"
+        LogTraceMessage "Variable SiteServer set to $script:SiteServer"
+        Write-Verbose "Variable SiteServer set to $script:SiteServer"
+
+        $Script:SiteCode = "TEE"
+        LogTraceMessage "Variable SiteCode set to $script:SiteCode"
+        Write-Verbose "Variable SiteCode set to $script:SiteCode"
+
+        $Script:ScriptPath = "\\TEE-CM1.EmptyGarden.btls\SCCMPackages$\OSD - Get-DriverPackage\Set-DriverPackageXML.ps1"
+        LogTraceMessage "Variable ScriptPath set to $script:ScriptPath"
+        Write-Verbose "Variable ScriptPath set to $script:ScriptPath"
+
+ 	    # Function Finished
+		LogTraceMessage "*** Function SetGlobalEnvVariables Finished ***"
+        Write-Verbose "*** Function SetGlobalEnvVariables Finished ***"
+	}
+
+	Function ImportPsModules
+	{
+		# Function Started
+        LogTraceMessage "*** Function ImportPsModules Started ***"
+        Write-Verbose "*** Function ImportPsModules Started ***"
+
+      	# Function Finished
+		LogTraceMessage "*** Function ImportPsModules Finished ***"
+        Write-Verbose "*** Function ImportPsModules Finished ***"
+	}
+
+    Function WMI-InstanceFilter
+    {
+    	# Function Started
+        LogTraceMessage "*** Function WMI-InstanceFilter Started ***"
+        Write-Verbose "*** Function WMI-InstanceFilter Started ***"
+
+        $PropertyHash = @{
+            QueryLanguage = "WQL";
+            Query = "SELECT * FROM __InstanceOperationEvent Within 60 Where TargetInstance ISA 'SMS_Package' and TargetInstance.Name like 'Drivers - %'";
+            Name = "SCCM_DriverUpdateXML";
+            EventNameSpace="root/sms/site_$($SiteCode)"
+            }
+
+        $Script:InstanceFilter = New-CimInstance -Namespace root/subscription -ClassName __EventFilter -Property $PropertyHash -Verbose -ErrorAction Stop 
+
+
+      	# Function Finished
+		LogTraceMessage "*** Function WMI-InstanceFilter Finished ***"
+        Write-Verbose "*** Function WMI-InstanceFilter Finished ***"
+    }
+
+    Function WMI-InstanceConsumer
+    {
+    	# Function Started
+        LogTraceMessage "*** Function WMI-InstanceConsumer Started ***"
+        Write-Verbose "*** Function WMI-InstanceConsumer Started ***"
+
+        $PropertyHash =@{
+            Name = "SCCM_DriverUpdateXML";
+            ExecutablePath = "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe";
+            CommandLineTemplate="C:\Windows\\System32\WindowsPowerShell\v1.0\powershell.exe -ExecutionPolicy Bypass -file `"$ScriptPath`""
+            }
+
+        $Script:InstanceConsumer = New-CimInstance -Namespace root/subscription -ClassName CommandLineEventConsumer -Property $PropertyHash -Verbose -ErrorAction Stop
+
+      	# Function Finished
+		LogTraceMessage "*** Function WMI-InstanceConsumer Finished ***"
+        Write-Verbose "*** Function WMI-InstanceConsumer Finished ***"
+    }
+
+    Function WMI-FilterToConsumerBinding
+    {
+    	# Function Started
+        LogTraceMessage "*** Function WMI-FilterToConsumerBinding Started ***"
+        Write-Verbose "*** Function WMI-FilterToConsumerBinding Started ***"
+
+        $PropertyHash = @{
+            Filter = [ref]$Script:InstanceFilter; 
+            Consumer = [ref]$Script:InstanceConsumer
+        }
+         
+        $InstanceBinding= New-CimInstance -Namespace root/subscription -ClassName __FilterToConsumerBinding  -Property $PropertyHash -Verbose -ErrorAction Stop
+
+      	# Function Finished
+		LogTraceMessage "*** Function WMI-FilterToConsumerBinding Finished ***"
+        Write-Verbose "*** Function WMI-FilterToConsumerBinding Finished ***"
+    }
+
+ 	Function LogTraceMessage ($strMessage)
+	{
+		[array]$script:TraceMessage += (Get-Date).ToString() + ':  ' + $strMessage + '~~' 
+    }
+    
+ 	# Script Started
+	LogTraceMessage "*** Get-DriverPacakge ***"
+    Write-Verbose "*** Get-DriverPacakge ***"
+
+	#Main
+	Main
+	
+}
+
+Catch
+{
+	# Catch Started
+	LogTraceMessage "*** Catch Started ***"
+    Write-Verbose "*** Catch Started ***"
+	
+	# Log error messages
+	$script:ErrorMessage = $Error[0].Exception.ToString()
+	LogTraceMessage "Variable ErrorMessage set to $script:ErrorMessage"
+    Write-Verbose "Variable ErrorMessage set to $script:ErrorMessage"
+	
+	$script:ErrorState = 3
+	LogTraceMessage "Variable ErrorState set to $script:ErrorState"
+    Write-Verbose "Variable ErrorState set to $script:ErrorState"
+	
+	# Catch Finished
+	LogTraceMessage "*** Catch Finished ***"
+    Write-Verbose "*** Catch Finished ***"
+}
+
+Finally
+{
+	# Finally Started
+	LogTraceMessage "*** Finally Started ***"
+    Write-Verbose "*** Finally Started ***" 
+
+	# Log Error State/Message
+	LogTraceMessage "Variable ErrorState = $script:ErrorState"
+    Write-Verbose  "Variable ErrorState = $script:ErrorState"
+	
+	# Finally Finished
+	LogTraceMessage "*** Finally Finished ***"
+	Write-Verbose "*** Finally Finished ***"
+	# Script Finished
+	LogTraceMessage "*** Get-DriverPacakge Finished ***"
+    Write-Verbose "*** Get-DriverPacakgefinished Finished ***"
+
+    # Write to log file
+    $script:TraceMessage | Out-File $script:LoggingPath
+
+}
