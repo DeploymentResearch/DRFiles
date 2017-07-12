bad3f4dab980a6d076c1e40b0109980bcde1b9e7
arwidmark <arwidmark@gmail.com>
2017-07-12 14:50:49 -0500

    Added script to set AD permissions

---
+++ Scripts/Set-OUPermissions.ps1
@@ -0,0 +1,60 @@
+﻿<#
+Created:	 2013-01-08
+Version:	 1.0
+Author       Mikael Nystrom and Johan Arwidmark       
+Homepage:    http://www.deploymentfundamentals.com
+
+Disclaimer:
+This script is provided "AS IS" with no warranties, confers no rights and 
+is not supported by the authors or DeploymentArtist.
+
+Author - Mikael Nystrom
+    Twitter: @mikael_nystrom
+    Blog   : http://deploymentbunny.com
+
+Author - Johan Arwidmark
+    Twitter: @jarwidmark
+    Blog   : http://deploymentresearch.com
+#>
+
+Param
+(
+[parameter(mandatory=$true,HelpMessage="Please, provide the account name.")][ValidateNotNullOrEmpty()]$Account,
+[parameter(mandatory=$true,HelpMessage="Please, provide the target OU.")][ValidateNotNullOrEmpty()]$TargetOU
+)
+
+# Start logging to screen
+Write-host (get-date -Format u)" - Starting"
+
+# This i what we typed in
+Write-host "Account to search for is" $Account
+Write-Host "OU to search for is" $TargetOU
+
+if ($TargetOU -like '*dc=*')
+{ 
+    Write-Warning "Oupps, only specify the OU path. We get the domain for you..."
+    Break
+} 
+
+$CurrentDomain = Get-ADDomain
+
+$OrganizationalUnitDN = $TargetOU+","+$CurrentDomain
+$SearchAccount = Get-ADUser $Account
+
+$SAM = $SearchAccount.SamAccountName
+$UserAccount = $CurrentDomain.NetBIOSName+"\"+$SAM
+
+Write-Host "Account is = $UserAccount"
+Write-host "OU is =" $OrganizationalUnitDN
+
+dsacls.exe $OrganizationalUnitDN /G $UserAccount":CCDC;Computer" /I:T | Out-Null
+dsacls.exe $OrganizationalUnitDN /G $UserAccount":LC;;Computer" /I:S | Out-Null
+dsacls.exe $OrganizationalUnitDN /G $UserAccount":RC;;Computer" /I:S | Out-Null
+dsacls.exe $OrganizationalUnitDN /G $UserAccount":WD;;Computer" /I:S  | Out-Null
+dsacls.exe $OrganizationalUnitDN /G $UserAccount":WP;;Computer" /I:S  | Out-Null
+dsacls.exe $OrganizationalUnitDN /G $UserAccount":RP;;Computer" /I:S | Out-Null
+dsacls.exe $OrganizationalUnitDN /G $UserAccount":CA;Reset Password;Computer" /I:S | Out-Null
+dsacls.exe $OrganizationalUnitDN /G $UserAccount":CA;Change Password;Computer" /I:S | Out-Null
+dsacls.exe $OrganizationalUnitDN /G $UserAccount":WS;Validated write to service principal name;Computer" /I:S | Out-Null
+dsacls.exe $OrganizationalUnitDN /G $UserAccount":WS;Validated write to DNS host name;Computer" /I:S | Out-Null
+dsacls.exe $OrganizationalUnitDN
