03679bcc39c75b1e4d830979ba556f2abb59b88f
arwidmark <arwidmark@gmail.com>
2017-09-06 10:09:49 -0500

    Sample PowerShell code to create shortcut on Desktop

---
+++ Scripts/CreateShortCutOnDesktop.ps1
@@ -0,0 +1,10 @@
+﻿# Sample code to create shortcut on Desktop 
+
+$IconLocation = "$Env:USERPROFILE\Desktop\Icons-Land-Multiple-Smiley-Pirate-Smile.ico"
+If (!(test-path $IconLocation)){ Write-warning "Oops, icon file is missing, aborting..."; Break }
+$WshShell = New-Object -ComObject "WScript.Shell"
+$ShortCut = $WshShell.CreateShortcut("$Env:USERPROFILE\Desktop\Link.lnk")
+$ShortCut.TargetPath = 'https://google.com'
+$ShortCut.IconLocation = $IconLocation
+$ShortCut.Save()
+
