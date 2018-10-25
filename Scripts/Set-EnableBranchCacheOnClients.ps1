e4e64553c30d0e8c08292caf159c665000d7bad9
arwidmark <arwidmark@gmail.com>
2018-10-26 00:54:04 +0200

    Added script to enable BranchCache

---
+++ Scripts/Set-EnableBranchCacheOnClients.ps1
@@ -0,0 +1,25 @@
+﻿# Script to enable BranchCache on Windows 7 and Windows 10 clients
+
+$BCPort = "1337"
+$TTL = "365"
+
+# Reset BranchCache
+netsh branchcache reset
+
+# Set BranchCache ConnectPort
+REG ADD "HKLM\Software\Microsoft\Windows NT\CurrentVersion\PeerDist\DownloadManager\Peers\Connection" /v ConnectPort /t REG_DWORD /d $BCPort /f
+
+# Set BranchCache ListenPor
+REG ADD "HKLM\Software\Microsoft\Windows NT\CurrentVersion\PeerDist\DownloadManager\Peers\Connection" /v ListenPort /t REG_DWORD /d $BCPort /f
+
+# Set BranchCache Cache Time To Live for cached data
+REG ADD "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\PeerDist\Retrieval" /v SegmentTTL /t REG_DWORD /d $TTL /f
+
+# Enable BranchCache in Distributed Mode and to serve peers with content while on battery
+netsh branchcache set service mode=distributed serveonbattery=true
+
+# Set BranchCache Cache Size to 50% of disk space
+netsh branchcache set cachesize size=50 percent=TRUE
+
+# Set BranchCache service start mode to Automatic
+Set-Service –Name peerdistsvc –StartupType Automatic
