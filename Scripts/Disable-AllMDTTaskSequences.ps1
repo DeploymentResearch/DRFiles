e5b533dc0650dfce9ef643e7bbbae04b37a4043e
arwidmark <arwidmark@gmail.com>
2019-11-29 09:17:02 -0600

    Create Disable-AllMDTTaskSequences.ps1

---
+++ Scripts/Disable-AllMDTTaskSequences.ps1
@@ -0,0 +1,20 @@
+﻿# NOTE: Close the Deployment Workbench prior to running this
+
+# Specify TaskSequences.xml to use
+$TSXMLFile = "C:\DeploymentShare\Control\TaskSequences.xml"
+ 
+# Make a backup copy just in case
+$TimeStamp = "_"+$(get-date -f MMddyyyy)+"_"+$(get-date -f HHmmss)
+Copy-Item $TSXMLFile $TSXMLFile$TimeStamp
+
+# Load the TaskSequences.xml file as a XML object
+[xml]$TSXML = Get-Content -Path $TSXMLFile
+
+# Enable all task sequences
+foreach ($TS in $TSXML.tss.ts)
+{
+    $TS.enable = "False"
+}
+
+# Save the changes
+$TSXML.Save($TSXMLFile)
