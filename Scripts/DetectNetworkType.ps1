c5ded1c247580617f059faff9a83bfa8af4d193b
arwidmark <arwidmark@gmail.com>
2019-12-19 18:54:04 -0600

    Create DetectNetworkType.ps1

---
+++ Scripts/DetectNetworkType.ps1
@@ -0,0 +1,79 @@
+﻿#Get Connection Type
+$WirelessConnected = $null
+$WiredConnected = $null
+$VPNConnected = $null
+
+# Detecting PowerShell version, and call the best cmdlets
+if ($PSVersionTable.PSVersion.Major -gt 2)
+{
+    # Using Get-CimInstance for PowerShell version 3.0 and higher
+    $WirelessAdapters =  Get-CimInstance -Namespace "root\WMI" -Class MSNdis_PhysicalMediumType -Filter `
+        'NdisPhysicalMediumType = 9'
+    $WiredAdapters = Get-CimInstance -Namespace "root\WMI" -Class MSNdis_PhysicalMediumType -Filter ` 
+        "NdisPhysicalMediumType = 0 and `
+        (NOT InstanceName like '%pangp%') and `
+        (NOT InstanceName like '%cisco%') and `
+        (NOT InstanceName like '%juniper%') and `
+        (NOT InstanceName like '%vpn%') and `
+        (NOT InstanceName like 'Hyper-V%') and `
+        (NOT InstanceName like 'VMware%') and `
+        (NOT InstanceName like 'VirtualBox Host-Only%')" 
+    $ConnectedAdapters =  Get-CimInstance -Class win32_NetworkAdapter -Filter `
+        'NetConnectionStatus = 2'
+    $VPNAdapters =  Get-CimInstance -Class Win32_NetworkAdapterConfiguration -Filter `
+        "Description like '%pangp%' ` 
+        or Description like '%cisco%'  `
+        or Description like '%juniper%' `
+        or Description like '%vpn%'" 
+}
+else
+{
+    # Needed this script to work on PowerShell 2.0 (don't ask)
+    $WirelessAdapters = Get-WmiObject -Namespace "root\WMI" -Class MSNdis_PhysicalMediumType -Filter `
+        'NdisPhysicalMediumType = 9'
+    $WiredAdapters = Get-WmiObject -Namespace "root\WMI" -Class MSNdis_PhysicalMediumType -Filter `
+        "NdisPhysicalMediumType = 0 and `
+        (NOT InstanceName like '%pangp%') and `
+        (NOT InstanceName like '%cisco%') and `
+        (NOT InstanceName like '%juniper%') and `
+        (NOT InstanceName like '%vpn%') and `
+        (NOT InstanceName like 'Hyper-V%') and `
+        (NOT InstanceName like 'VMware%') and `
+        (NOT InstanceName like 'VirtualBox Host-Only%')" 
+    $ConnectedAdapters = Get-WmiObject -Class win32_NetworkAdapter -Filter `
+        'NetConnectionStatus = 2'
+    $VPNAdapters = Get-WmiObject -Class Win32_NetworkAdapterConfiguration -Filter `
+        "Description like '%pangp%' ` 
+        or Description like '%cisco%'  `
+        or Description like '%juniper%' `
+        or Description like '%vpn%'" 
+}
+
+
+Foreach($Adapter in $ConnectedAdapters) {
+    If($WirelessAdapters.InstanceName -contains $Adapter.Name)
+    {
+        $WirelessConnected = $true
+    }
+}
+
+Foreach($Adapter in $ConnectedAdapters) {
+    If($WiredAdapters.InstanceName -contains $Adapter.Name)
+    {
+        $WiredConnected = $true
+    }
+}
+
+Foreach($Adapter in $ConnectedAdapters) {
+    If($VPNAdapters.Index -contains $Adapter.DeviceID)
+    {
+        $VPNConnected = $true
+    }
+}
+
+If(($WirelessConnected -ne $true) -and ($WiredConnected -eq $true)){ $ConnectionType="WIRED"}
+If(($WirelessConnected -eq $true) -and ($WiredConnected -eq $true)){$ConnectionType="WIRED AND WIRELESS"}
+If(($WirelessConnected -eq $true) -and ($WiredConnected -ne $true)){$ConnectionType="WIRELESS"}
+If($VPNConnected -eq $true){$ConnectionType="VPN"}
+
+Write-Output "Connection type is: $ConnectionType"
