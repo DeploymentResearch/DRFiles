9f78c73aec8ed2d758efd9058d0a88c69f10b3a8
arwidmark <arwidmark@gmail.com>
2020-05-18 16:04:26 -0500

    Create Enable-PromiscuousModeHyper-V.ps1

---
+++ Scripts/Enable-PromiscuousModeHyper-V.ps1
@@ -0,0 +1,10 @@
+﻿# Enable Promiscuous Mode for an external virtual switch in Hyper-V
+$VMSwitch = "Chicago1"
+$portFeature = Get-VMSystemSwitchExtensionPortFeature -FeatureName "Ethernet Switch Port Security Settings"
+# None = 0, Destination = 1, Source = 2
+$portFeature.SettingData.MonitorMode = 2
+Add-VMSwitchExtensionPortFeature -ExternalPort -SwitchName $VMSwitch -VMSwitchExtensionFeature $portFeature
+
+# Show settings
+Get-VMSwitchExtensionPortFeature -ExternalPort -SwitchName $VMSwitch –FeatureName "Ethernet Switch Port Security Settings"
+Get-VMSwitchExtensionPortFeature -ExternalPort -SwitchName $VMSwitch | select -ExpandProperty SettingData
