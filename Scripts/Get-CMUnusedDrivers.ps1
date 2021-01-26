03c77c029116443b3c59b33df7d1097d520511aa
arwidmark <arwidmark@gmail.com>
2021-01-26 08:23:47 -0600

    Update Get-CMUnusedDrivers.ps1

--- Scripts/Get-CMUnusedDrivers.ps1
+++ Scripts/Get-CMUnusedDrivers.ps1
@@ -1,4 +1,8 @@
-﻿<#.Synopsis   This script queries Configuration Manager 2012 Drivers that are not related with any Driver Packages.DESCRIPTION.EXAMPLE
+﻿<#
+.Synopsis
+   This script queries Configuration Manager 2012 Drivers that are not related with any Driver Packages
+.DESCRIPTION
+.EXAMPLE
     Get-CMUnusedDrivers.ps1 -SiteCode PS1 -SiteServer CM01
 .NOTES
     Developed By Johan Arwidmark and Kaido Järvemets
@@ -14,10 +18,10 @@ Param(
 
 Try{
     $DriverAr = @()
-    $Drivers = Get-WmiObject -Namespace "Root\SMS\Site_$($SiteCode)" -Class SMS_Driver -ErrorAction STOP -ComputerName $SiteServer
+    $Drivers = Get-WmiObject -Namespace "RootSMSSite_$($SiteCode)" -Class SMS_Driver -ErrorAction STOP -ComputerName $SiteServer
     foreach($Item in $Drivers){
         Try{
-            $Query = Get-WmiObject -Namespace "Root\SMS\Site_$($SiteCode)" -Query "select * from SMS_Driver where CI_ID not in(select CI_ID from SMS_DriverContainer where CI_ID='$($item.CI_ID)') and CI_ID='$($item.CI_ID)'" -ErrorAction STOP -ComputerName $SiteServer
+            $Query = Get-WmiObject -Namespace "RootSMSSite_$($SiteCode)" -Query "select * from SMS_Driver where CI_ID not in(select CI_ID from SMS_DriverContainer where CI_ID='$($item.CI_ID)') and CI_ID='$($item.CI_ID)'" -ErrorAction STOP -ComputerName $SiteServer
                 if(($Query | Measure-Object | Select-Object -ExpandProperty Count) -ne 0){
                     $DObject = New-Object PSOBJECT
                         $DObject | Add-Member -MemberType NoteProperty -Name "CI_ID" -Value $Query.CI_ID
@@ -29,11 +33,10 @@ Try{
         Catch{
             $_.Exception.Message
         }
-    
+
     }
     $DriverAr
 }
 Catch{
     $_.Exception.Message
 }
-
