9fc96e5f9fa6a6cb82018c9285b469256c1dab95
arwidmark <arwidmark@gmail.com>
2021-02-09 09:38:37 -0600

    Blog sample scripts

---
+++ Scripts/CompareESDtoWIMBlog/GetNetworkInfoFromVirtualRouterSeparatedBySubnet.ps1
@@ -0,0 +1,40 @@
+﻿# These sample assumes you have added additional meter actions to the network devices
+# Add-VMNetworkAdapterAcl –VMName $VMName –Action Meter –Direction Both –RemoteIpAddress 192.168.2.0/24
+# Add-VMNetworkAdapterAcl –VMName $VMName –Action Meter –Direction Both –RemoteIpAddress 192.168.28.0/24
+#
+# I also used this command to list the switch names
+# Get-VMNetworkAdapter -VMName GW02-PFSENSE | select SwitchName, IPAddresses, id
+
+$VMName = "GW02-PFSENSE"
+$Logfile = "C:\Setup\Lab-Networkinfo-IPU-GW02.log"
+$TimeInBetweenTests = 10 # Seconds
+$NumberOfTests = '1440' # One test per 10 second = 4 hours
+
+# Enable monitoring and reset the counters
+Get-VM -Name $VMName | Enable-VMResourceMetering
+Get-VM -Name $VMName | Reset-VMResourceMetering
+
+# Remove any existing logfile
+If (test-path $Logfile){ Remove-Item $Logfile -Force }
+
+$i = 1
+do {
+
+    # Set TimeStamp to measurement of first VM in each test
+    $Time = $(TimeStamp)
+    $TimeFormatted = $Time.ToUniversalTime().ToString("MM/dd/yy HH:mm:ss")
+
+    $TimeFormatted + " ---------------------------------------------------------------------------------------------" | Out-File -FilePath $Logfile -Append -Encoding ascii
+    $TimeFormatted + " Internet switch (192.168.30.3) is Microsoft:885BB2EB-7883-4D54-B433-BB89C51142F4\B3F811C3-A386-4F63-BB99-3675A4DD8322" | Out-File -FilePath $Logfile -Append -Encoding ascii
+    $TimeFormatted + " Stockholm switch (192.168.2.1) is Microsoft:885BB2EB-7883-4D54-B433-BB89C51142F4\E65D030F-1964-4748-B77F-D7D59FF91E58" | Out-File -FilePath $Logfile -Append -Encoding ascii
+    $TimeFormatted + " Liverpool switch (192.168.28.1) is Microsoft:885BB2EB-7883-4D54-B433-BB89C51142F4\38C5D50E-C1D6-4859-97D5-91F21DE03B0E" | Out-File -FilePath $Logfile -Append -Encoding ascii
+    $TimeFormatted + " ---------------------------------------------------------------------------------------------" | Out-File -FilePath $Logfile -Append -Encoding ascii
+
+    Get-VMNetworkAdapterAcl -VMName $VMName | Out-File -FilePath $Logfile -Append -Encoding ascii
+
+    # Sleep in between tests
+    Start-Sleep -Seconds $TimeInBetweenTests
+
+$i++
+}
+while ($i -le $NumberOfTests)
---
+++ Scripts/CompareESDtoWIMBlog/GetNetworkInfoGeneric.ps1
@@ -0,0 +1,87 @@
+﻿# Script to measure outbound traffic on a set of Hyper-V VMs, both individual and summarized for all VMs
+
+$Logfile = "C:\Setup\Lab-Networkinfo-ESD-Testing.log"
+$TimeInBetweenTests = 10 # Seconds
+$NumberOfTests = '1440' # One test per 10 second = 4 hours
+
+# VMs to measure outbound traffic for. 
+# In this example a Domain Controller, a ConfigMgr Site Server, and a ConfigMgr DP
+$VMsToMeasure = "DEMO-OSD-TP-DP01","GW02-PFSENSE","DEMO-OSD-TP-TEST01","DEMO-OSD-TP-TEST02","DEMO-OSD-TP-TEST03"
+
+# Enable Resource Metering on the selected VMs
+Get-VM $VMsToMeasure | Enable-VMResourceMetering
+
+Function TimeStamp {
+    # $(Get-Date -UFormat "%D %T")
+    #(Get-Date).ToUniversalTime().ToString("MM/dd/yy HH:mm:ss")
+    Get-Date
+} 
+
+Function GetInboundTraffic{
+    Param($VMName)
+    $networkinfo = Measure-VM -Name $VMName | `
+    Select-Object -property @{Expression = {"{0:N2}" -f(($_.NetworkMeteredTrafficReport | `
+    Where-Object direction -Eq 'Inbound' | `
+    Measure-Object -property TotalTraffic -sum).Sum / 1024) };Label="Inbound Network Traffic (GB)"}
+    return $networkinfo.'Inbound Network Traffic (GB)'
+}
+
+Function GetOutboundTraffic{
+    Param($VMName)
+    $networkinfo = Measure-VM -Name $VMName | `
+    Select-Object -property @{Expression = {"{0:N2}" -f(($_.NetworkMeteredTrafficReport | `
+    Where-Object direction -Eq 'outbound' | `
+    Measure-Object -property TotalTraffic -sum).Sum / 1024) };Label="Outbound Network Traffic (GB)"}
+    return $networkinfo.'Outbound Network Traffic (GB)'
+}
+
+# Remove any existing logfile
+If (test-path $Logfile){ Remove-Item $Logfile -Force }
+
+# Reset the metering counters for all measured VMs
+Get-VM -Name $VMsToMeasure | Reset-VMResourceMetering
+
+# Set Starttime for calcultation duration
+$StartTime = $(TimeStamp)
+
+$i = 1
+do {
+
+    # Set TimeStamp to measurement of first VM in each test
+    $Time = $(TimeStamp)
+    $TimeFormatted = $Time.ToUniversalTime().ToString("MM/dd/yy HH:mm:ss")
+    
+    [System.Collections.ArrayList]$AggregateTraffic = @()
+    # Measure each VM
+    Foreach($VM in $VMsToMeasure){
+
+        $InboundTraffic = (GetInboundTraffic -VMName $VM)
+        $TimeFormatted + " $VM Inbound Network Traffic (GB) : $InboundTraffic" | Out-File -FilePath $Logfile -Append -Encoding ascii
+        
+        $OutboundTraffic = (GetOutboundTraffic -VMName $VM)
+        $TimeFormatted + " $VM Outbound Network Traffic (GB) : $OutboundTraffic" | Out-File -FilePath $Logfile -Append -Encoding ascii
+
+        $obj = [PSCustomObject]@{
+
+            # Add values to arraylist
+            VMName = $VM
+            TimeStamp = $TimeFormatted 
+            OutboundTraffic = $OutboundTraffic
+            InboundTraffic = $InboundTraffic
+            
+        }
+        
+        # Add all the values
+        $AggregateTraffic.Add($obj)|Out-Null
+
+    }
+
+    # Log duration
+    $Duration = New-TimeSpan –Start $StartTime –End $Time
+    "Duration so far is: $($Duration.Hours) hours, $($Duration.Minutes) minutes, $($Duration.Seconds) seconds" | Out-File -FilePath $Logfile -Append -Encoding ascii
+
+    # Sleep in between tests
+    Start-Sleep -Seconds $TimeInBetweenTests
+$i++
+}
+while ($i -le $NumberOfTests)
