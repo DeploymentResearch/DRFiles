4988d8c188d6661089cd9cadeb5e359daa1f9752
arwidmark <arwidmark@gmail.com>
2021-05-07 15:32:26 -0500

    Create 4_Get-AutopilotHardwareHash.ps1

---
+++ Scripts/CloudImagingPart4/4_Get-AutopilotHardwareHash.ps1
@@ -0,0 +1,13 @@
+﻿# Install Nuget
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
+
+# Create working folder
+$AutopilotFolder = "C:\AutoPilot"
+If (!(Test-Path $AutopilotFolder)){ New-Item $AutopilotFolder -ItemType Directory -Force }
+
+# Save Autopilot script
+Save-Script -Name Get-WindowsAutoPilotInfo -Path $AutopilotFolder
+
+# Get the hardware hash 
+& "$AutopilotFolder\Get-WindowsAutoPilotInfo.ps1"-OutputFile "$AutopilotFolder\$($env:ComputerName)_HWID.csv"
