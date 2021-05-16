70577a5c62860bfc89b520e1f6f929cb2c756791
arwidmark <arwidmark@gmail.com>
2021-05-16 12:38:37 -0500

    Create Import-TaskSequenceSyntax.ps1

---
+++ Scripts/ImportExportConfigMgrTaskSequences/Import-TaskSequenceSyntax.ps1
@@ -0,0 +1,6 @@
+﻿# Syntax example for Import-TaskSequence.ps1 scipt
+$TSImportFile = "E:\Demo\ExportedTaskSequences\PS100320.xml"
+$NewTSName = "Windows 10 Enterprise x64 20HD From Export"
+
+Set-Location "E:\Demo\Import and Export TS"
+.\Import-TaskSequence.ps1 -SiteCode PS1 -TaskSequenceName $NewTSName -InputFile $TSImportFile 
