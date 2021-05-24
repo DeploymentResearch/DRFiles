685bce924aa7f41d42a67f85b80f94346194b6fd
arwidmark <arwidmark@gmail.com>
2021-05-24 09:18:47 -0500

    Create Get-CMCacheInfo.ps1

---
+++ Scripts/Get-CMCacheInfo.ps1
@@ -0,0 +1,14 @@
+﻿$resman = new-object -com "UIResource.UIResourceMgr"; $cacheInfo = $resman.GetCacheInfo()
+
+$ccmcachetotal = ($cacheinfo.TotalSize)/1024
+$ccmcachetotal = [math]::Round($ccmcachetotal,2)
+
+$ccmcachefree = ($cacheinfo.FreeSize)/1024
+$ccmcachefree = [math]::Round($ccmcachefree,2)
+
+$ccmcacheused = $ccmcachetotal - $ccmcachefree
+$ccmcacheused = [math]::Round($ccmcacheused,2)
+
+Write-Host "Total Cache Space: $ccmcachetotal GB"
+Write-Host "Used Cache Space: $ccmcacheused GB"
+Write-Host "Free Cache Space: $ccmcachefree GB"
