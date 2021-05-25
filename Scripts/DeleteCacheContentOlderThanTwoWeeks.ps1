5457e97065eba66918f4a8f9add21e0a197d9369
arwidmark <arwidmark@gmail.com>
2021-05-25 10:06:20 -0500

    Create DeleteCacheContentOlderThanTwoWeeks.ps1

---
+++ Scripts/DeleteCacheContentOlderThanTwoWeeks.ps1
@@ -0,0 +1,10 @@
+﻿
+$MinDays = 14
+$UIResourceMgr = New-Object -ComObject UIResource.UIResourceMgr
+$Cache = $UIResourceMgr.GetCacheInfo()
+
+$Cache.GetCacheElements() |
+where-object {[datetime]$_.LastReferenceTime -lt (get-date).adddays(-$MinDays)} |
+foreach {
+    $Cache.DeleteCacheElement($_.CacheElementID)
+}
