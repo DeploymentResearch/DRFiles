91d3bfbb19e825444ef326640589e156888c93f0
arwidmark <arwidmark@gmail.com>
2021-05-25 21:56:08 -0500

    Create Export-WindowsServer2019WIMfromISO.ps1

---
+++ Scripts/Export-WindowsServer2019WIMfromISO.ps1
@@ -0,0 +1,14 @@
+﻿# Script to extract the Windows Server 2019 Standard index from a Windows Server 2019 media.
+# Update line 4 and 5 to match your environment
+
+$ISO = "E:\iso\Windows Server 2019 (updated May 2021).iso" # Path to Windows Server 2019 media
+$WIMFolder = "C:\WIM" # Target folder for extracted WIM file containg Windows Server 2019 Standard only
+$WIMFile = "$WIMFolder\REFWS2019-001.wim"
+$Edition = "Windows Server 2019 Standard (Desktop Experience)" 
+
+Mount-DiskImage -ImagePath $ISO | Out-Null
+$ISOImage = Get-DiskImage -ImagePath $ISO | Get-Volume
+$ISODrive = [string]$ISOImage.DriveLetter+":"
+If (!(Test-path $WIMPath)){ New-Item -Path $WIMPath -ItemType Directory -Force | Out-Null } # Create folder if needed
+Export-WindowsImage -SourceImagePath "$ISODrive\sources\install.wim" -SourceName $Edition -DestinationImagePath $WIMFile
+Dismount-DiskImage -ImagePath $ISO | Out-Null
