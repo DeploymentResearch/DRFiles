69e17e259c852eec85635f99af1eb31efe1b45f1
arwidmark <arwidmark@gmail.com>
2021-07-16 05:44:44 -0500

    Added iperf scripts

---
+++ Scripts/iPerf/Remove-iPerf3Firewall.ps1
@@ -0,0 +1,3 @@
+﻿# Remove iPerf3 firewall rules
+Remove-NetFirewallRule -DisplayName "iPerf3 Server Inbound TCP Rule"
+Remove-NetFirewallRule -DisplayName "iPerf3 Server Outbound TCP Rule"
---
+++ Scripts/iPerf/Set-iPerf3Firewall.ps1
@@ -0,0 +1,30 @@
+﻿$Port = "5201"
+
+$FirewallInboundParams = @{
+    DisplayName = "iPerf3 Server Inbound TCP Rule";
+    Direction = "Inbound";
+    LocalPort = $Port;
+    Protocol = "TCP";
+    Action = "Allow";
+    ErrorAction = "SilentlyContinue";
+}
+
+$FirewallOutboundParams = @{
+    DisplayName = "iPerf3 Server Outbound TCP Rule";
+    Direction = "Outbound";
+    LocalPort = $Port;
+    Protocol = "TCP";
+    Action = "Allow";
+    ErrorAction = "SilentlyContinue";
+}
+
+$inboundResult = New-NetFirewallRule @FirewallInboundParams
+$outboundResult = New-NetFirewallRule @FirewallOutboundParams
+
+if ($inboundResult -and $outboundResult) {
+    Write-Verbose -Message 'iPerf3 server port firewall rules set.'
+}
+else {
+    throw "iPerf3 server port firewall rules could not be set. Message: {0}" -f $error[0].Exception.message
+}
+
