7da96ec440d235c73b8930624ba0fceff48e7173
arwidmark <arwidmark@gmail.com>
2021-07-30 09:46:27 -0500

    Create RemoveComputerFromDomain.ps1

---
+++ Scripts/RemoveComputerFromDomain.ps1
@@ -0,0 +1,11 @@
+﻿# Set credentials and allow remote administration via PowerShell to all hosts
+Set-Item WSMan:\localhost\Client\TrustedHosts -Value "*" -Force
+
+$Username = 'VIAMONSTRA\Administrator'
+$Password = 'P@ssw0rd'
+$pass = ConvertTo-SecureString -AsPlainText $Password -Force
+$UnjoinCred = New-Object System.Management.Automation.PSCredential -ArgumentList $Username,$pass
+
+Remove-Computer -UnjoinDomainCredential $UnjoinCred -Force -WorkgroupName "WORKGROUP"
+Start-sleep -Seconds 30
+Restart-Computer
