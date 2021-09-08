ec2a0c87a3d90dd9a22eb8c497c78b427815e82e
arwidmark <arwidmark@gmail.com>
2021-09-08 14:20:15 -0500

    Update Export-TaskSequence.ps1

--- Scripts/ImportExportConfigMgrTaskSequences/Export-TaskSequence.ps1
+++ Scripts/ImportExportConfigMgrTaskSequences/Export-TaskSequence.ps1
@@ -8,6 +8,6 @@ cd E:\Demo\ExportedTaskSequences
 $TsList = Get-WmiObject SMS_TaskSequencePackage -Namespace root\sms\site_$SiteCode
 ForEach ($Ts in $TsList)
  {
- $Ts = [wmi]“$($Ts.__PATH)”
-Set-Content -Path “$($ts.PackageId).xml” -Value $Ts.Sequence
+ $Ts = [wmi]"$($Ts.__PATH)"
+Set-Content -Path "$($ts.PackageId).xml" -Value $Ts.Sequence
  }
