ca91956e0888d991d2e0eb6270b20fb9b173d1ce
arwidmark <arwidmark@gmail.com>
2021-09-28 11:19:25 -0500

    Added MDT Monitoring

---
+++ Scripts/MDTMonitoring/GetMDTMonitorStatus.ps1
@@ -0,0 +1,26 @@
+﻿$URL = "http://MDT01:9801/MDTMonitorData/Computers"
+
+function GetMDTData {
+  $Data = Invoke-RestMethod $URL
+
+  foreach($property in ($Data.content.properties) ) {
+    New-Object PSObject -Property @{
+      Name = $($property.Name);
+      PercentComplete = $($property.PercentComplete.'#text');
+      Warnings = $($property.Warnings.'#text');
+      Errors = $($property.Errors.'#text');
+      DeploymentStatus = $(
+        Switch ($property.DeploymentStatus.'#text') {
+        1 { "Active/Running" }
+        2 { "Failed" }
+        3 { "Successfully completed" }
+        Default { "Unknown" }
+        }
+      );
+      StartTime = $($property.StartTime.'#text') -replace "T"," ";
+      EndTime = $($property.EndTime.'#text') -replace "T"," ";
+    }
+  }
+} 
+
+GetMDTData | Select Name, DeploymentStatus, PercentComplete, Warnings, Errors, StartTime, EndTime  | Out-GridView
