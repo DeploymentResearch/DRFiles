f8ac7ba462e6243d708059acdc12c2d933f5a6fd
arwidmark <arwidmark@gmail.com>
2021-10-21 10:53:59 -0500

    Added

---
+++ Scripts/ApplicationsAsWIMFiles/Install.ps1
@@ -0,0 +1,16 @@
+﻿$WimFile = Get-ChildItem Source.wim
+$MountPath = "C:\Users\Public\mount_" + $WimFile.BaseName
+$ScriptDir = split-path -parent $MyInvocation.MyCommand.Path
+
+# Mount the WIM file
+New-Item -Path $MountPath -ItemType Directory
+Mount-WindowsImage -ImagePath $wimFile.FullName -Index 1 -Path $mountPath
+
+# Install the app
+$SetupFile = "msiexec"
+$SetupSwitches = "/i $MountPath\snagit.msi /q"
+Start-Process -FilePath $SetupFile -ArgumentList $SetupSwitches -NoNewWindow -Wait
+    
+# Dismount the WIM file and Remove mount folder
+Dismount-WindowsImage -Path $MountPath -Discard
+Remove-Item -Path $MountPath -Force
---
+++ Scripts/ApplicationsAsWIMFiles/New-ApplicationWIM.ps1
@@ -0,0 +1,8 @@
+﻿# Creating a drive wim package manually
+$ApplicationsPath = "\\cm01\Sources\P2P Test Packages\300 MB Multiple Files"
+$TempSource = "E:\Temp\TempSource"
+$WimPathSource = "E:\Temp"
+
+Copy-Item -Path $ApplicationsPath -Destination $TempSource -Recurse
+
+New-WindowsImage -CapturePath $TempSource -ImagePath "$WimPathSource\Source.wim" -Name "Source"
