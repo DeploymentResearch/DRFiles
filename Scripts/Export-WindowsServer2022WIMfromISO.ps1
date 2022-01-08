fc7f71b0566117178f0db885a1fa9636e641388d
arwidmark <arwidmark@gmail.com>
2022-01-08 08:05:46 -0600

    Create Export-WindowsServer2022WIMfromISO.ps1

---
+++ Scripts/Export-WindowsServer2022WIMfromISO.ps1
@@ -0,0 +1,14 @@
+﻿# Script to extract the Windows Server 2022 Standard index from a Windows Server 2022 media.
+# Update line 4 and 5 to match your environment
+
+$ISO = "F:\ISO\Windows Server 2022).iso" # Path to Windows Server 2022 media
+$WIMPath = "C:\Ref" # Target folder for extracted WIM file containing Windows Server 2022 Standard only
+$WIMFile = "$WIMPath\REFWS2022-001.wim"
+$Edition = "Windows Server 2022 Standard (Desktop Experience)" 
+
+Mount-DiskImage -ImagePath $ISO | Out-Null
+$ISOImage = Get-DiskImage -ImagePath $ISO | Get-Volume
+$ISODrive = [string]$ISOImage.DriveLetter+":"
+If (!(Test-path $WIMPath)){ New-Item -Path $WIMPath -ItemType Directory -Force | Out-Null } # Create folder if needed
+Export-WindowsImage -SourceImagePath "$ISODrive\sources\install.wim" -SourceName $Edition -DestinationImagePath $WIMFile
+Dismount-DiskImage -ImagePath $ISO | Out-Null
