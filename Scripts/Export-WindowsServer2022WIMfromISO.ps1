6dfd10188cd06e3746ee0be144afce5e0ac5a481
Johan Arwidmark <arwidmark@gmail.com>
2022-04-15 11:26:17 -0500

    updates

--- Scripts/Export-WindowsServer2022WIMfromISO.ps1
+++ Scripts/Export-WindowsServer2022WIMfromISO.ps1
@@ -1,17 +1,15 @@
 ﻿# Script to extract the Windows Server 2022 Standard index from a Windows Server 2022 media.
-# Update line 5 - 9 to match your environment
-# Please note that editions in evaluation media are named differently, for example: Windows Server 2022 Standard Evaluation (Desktop Experience)
+# Update line 5 - 8 to match your environment
 
 # General parameteers
 $ISO = "F:\ISO\Windows Server 2022 (updated November 2021).iso" # Path to Windows Server 2022 media
 $WIMPath = "C:\Ref" # Target folder for extracted WIM file containing Windows Server 2022 Standard only
 $WIMFile = "$WIMPath\REFWS2022-001.wim" # Exported WIM File
-$Edition = "Windows Server 2022 Standard (Desktop Experience)" # Edition to export
+$Edition = "Windows Server 2022 Standard (Desktop Experience)" # Edition to export. Note: If using Evaluation Media, use: Windows Server 2022 Standard Evaluation (Desktop Experience)
 
 # Goal is to have a single index WIM File, so checking if target WIM File exist (if WIM exist, a new index will be appended)
 If (Test-path $WIMFile){
-    Write-Warning "WIM File: $WimFile does already exist"
-    Write-Warning "Rename or delete the file, then try again. Aborting..."
+    Write-Warning "WIM File: $WimFile does already exist. Rename or delete the file, then try again. Aborting..."
     Break 
 }
 
@@ -30,6 +28,7 @@ $ISODrive = [string]$ISOImage.DriveLetter+":"
 $SourceWIMFile = "$ISODrive\sources\install.wim"
 If (-not (Get-WindowsImage -ImagePath $SourceWIMFile | Where-Object {$_.ImageName -ilike "*$($Edition)"})){
     Write-Warning "WIM Edition: $Edition does not exist in WIM: $SourceWIMFile, aborting..."
+    Dismount-DiskImage -ImagePath $ISO | Out-Null
     Break
 }
 
