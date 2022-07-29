fcdf1db6e46c41abe1b12f4073861f5182e15891
Johan Arwidmark <arwidmark@gmail.com>
2022-07-29 11:28:50 -0500

    CPUUsage

---
+++ Scripts/CPU-PerProcessBenchmarking/Cancel-Downloads.ps1
@@ -0,0 +1,26 @@
+﻿#
+# Note: A user must be logged on for the script to work.
+# 
+
+# ConfigMgr Packages/Programs to run (ProgramID = Program Name)
+$Batch = @()
+$Batch += [pscustomobject]@{ PackageID = "PS100127"; ProgramID = "P2P Test Package - 2 GB Multiple Files" }
+
+# Run the programs
+[cimclass]$CimClass = (Get-CimClass -Namespace 'Root\ccm\clientsdk' -ClassName 'CCM_ProgramsManager' -ErrorAction 'Stop')
+foreach($Item in $Batch){
+
+    [hashtable]$Arguments = @{
+        'PackageID' = $Item.PackageID
+        'ProgramID' = $Item.ProgramID
+    }
+
+    Try {
+        Invoke-CimMethod -CimClass $CimClass -MethodName 'CancelDownload' –Arguments $Arguments -ErrorAction 'Stop'
+    }
+    Catch {
+        $ErrorMessage = "Could not cancel download"
+    }
+}
+
+
---
+++ Scripts/CPU-PerProcessBenchmarking/ClearConfigMgrAndBranchCacheCaches.ps1
@@ -0,0 +1,8 @@
+﻿# Clear ConfigMgr Cache
+$UIResourceMgr = New-Object -ComObject UIResource.UIResourceMgr
+$Cache = $UIResourceMgr.GetCacheInfo()
+$CacheElements = $Cache.GetCacheElements() 
+foreach ($Element in $CacheElements) { 	$Cache.DeleteCacheElementEx($Element.CacheElementID, $true) }
+
+# Clear BranchCache cache
+Clear-BCCache -Force
