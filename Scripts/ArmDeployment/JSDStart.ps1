a55b08e61ca2b3e491193aa71333550123375442
Johan Arwidmark <arwidmark@gmail.com>
2022-11-04 15:51:29 -0500

    Arm Deployment

---
+++ Scripts/ArmDeployment/JSDStart.ps1
@@ -0,0 +1,17 @@
+﻿# Deployment Server Share
+$DeployRoot = "\\DEV001\JurassicDeployment"
+
+# Prompt for username and password 
+$Cred = Get-Credential
+
+# Connect to the server
+try {
+	New-PSDrive -Name Z -PSProvider FileSystem -Root $DeployRoot -Credential $Cred
+}
+catch {
+	Write-Host "Could not connect to $DeployRoot, please run the JSDStart.ps1 script again"
+    Break
+}
+
+# Start main deployment script
+Z:\JSD.ps1
