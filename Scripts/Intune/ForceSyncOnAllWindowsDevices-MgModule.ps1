08708dd476052cdb59d047d75350e5a1957cd2ae
Andrew Johnson <15828090+AndrewJNet@users.noreply.github.com>
2022-11-17 10:16:03 -0600

    Create ForceSyncOnAllWindowsDevices-MgModule.ps1
    ForceSyncOnAllWindowsDevices.ps1, but leveraging the newer Microsoft.Graph module

---
+++ Scripts/Intune/ForceSyncOnAllWindowsDevices-MgModule.ps1
@@ -0,0 +1,41 @@
+#Requires -Modules Microsoft.Graph
+# Install the module. (You need admin on the machine.)
+# Install-Module Microsoft.Graph
+$TenantID = ""
+
+Select-MgProfile -Name "beta"
+$Tenant = Connect-MgGraph -TenantId $TenantID -Scopes "DeviceManagementManagedDevices.Read.All","DeviceManagementManagedDevices.ReadWrite.All"
+# Get all Windows Devices
+$Devices = Get-MgDeviceManagementManagedDevice -Filter "contains(operatingsystem, 'Windows')" | Get-MSGraphAllPages
+
+# Show Device Count
+($Devices | Measure-Object).Count
+
+# Report Last Sync, and force sync on each Device
+Foreach ($Device in $Devices)
+{
+    Write-Host "Last Sync Time was: $($Device.lastSyncDateTime)"
+    # Force sync
+    Sync-MgDeviceManagementManagedDevice -managedDeviceId $Device.managedDeviceId 
+    Write-Host "Sending Sync request to Device with DeviceID $($Device.managedDeviceId)" -ForegroundColor Yellow
+    Write-Host ""
+}
+
+
+#
+# Misc samples
+#
+
+# Get all devices
+Get-MgDeviceManagementManagedDevice | Get-MSGraphAllPages
+
+# Get devices for a specific user
+$Devices = Get-MgDeviceManagementManagedDevice | Where-Object {$_.userDisplayName -eq "Bob Smith"}
+$Devices | Select deviceName
+
+# Get Devices from wildcard 
+$Devices = Get-MgDeviceManagementManagedDevice -Filter "contains(deviceName, 'PC')"
+$Devices | Select deviceName
+
+# Get Single Device
+$Devices = Get-MgDeviceManagementManagedDevice -Filter "deviceName eq 'PC001'"
