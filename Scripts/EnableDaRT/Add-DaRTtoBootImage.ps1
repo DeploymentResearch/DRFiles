a48cd23cc09f61f40748628ef7632160bc1e1850
Johan Arwidmark <arwidmark@gmail.com>
2022-12-01 11:13:45 -0600

    minor update

--- Scripts/EnableDaRT/Add-DaRTtoBootImage.ps1
+++ Scripts/EnableDaRT/Add-DaRTtoBootImage.ps1
@@ -1,13 +1,4 @@
-﻿# Check for elevation
-Write-Host "Checking for elevation"
-
-If (-NOT ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole(`
-    [Security.Principal.WindowsBuiltInRole] "Administrator"))
-{
-    Write-Warning "Oupps, you need to run this script from an elevated PowerShell prompt!`nPlease start the PowerShell prompt as an Administrator and re-run the script."
-    Write-Warning "Aborting script..."
-    Break
-}
+﻿#Requires -RunAsAdministrator
 
 # Set some variables to resources
 $BootImageName = "Zero Touch WinPE 10 x64"
