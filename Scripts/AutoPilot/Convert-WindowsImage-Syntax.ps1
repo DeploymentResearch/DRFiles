3c020f56da4264b44d93d366574d64dba9e65e06
arwidmark <arwidmark@gmail.com>
2023-05-22 21:42:59 -0500

    Update Convert-WindowsImage-Syntax.ps1

--- Scripts/AutoPilot/Convert-WindowsImage-Syntax.ps1
+++ Scripts/AutoPilot/Convert-WindowsImage-Syntax.ps1
@@ -1,5 +1,5 @@
 ﻿# Convert a Windows 10 WIM file to VHDX file using Convert-WindowsImage.ps1 fork from https://github.com/nerdile/convert-windowsimage 
-$WimFile = "C:\WIM\REFW10-X64-21H2-Enterprise.wim"
+$WimFile = "C:\WIM\REFW10-X64-22H2-Enterprise.wim"
 $Edition = "Windows 10 Enterprise"
 $OutPutVHDXFile = "C:\VHDsV2\AP-W10-21H2.vhdx"
 
@@ -7,20 +7,10 @@ $OutPutVHDXFile = "C:\VHDsV2\AP-W10-21H2.vhdx"
 C:\Setup\Scripts\Convert-WindowsImage.ps1 -SourcePath $WimFile -Edition $Edition -VHDPath $OutPutVHDXFile -VHDFormat VHDX -VHDType Dynamic -VHDPartitionStyle GPT -SizeBytes 240GB
 
 
-# Convert a Windows 11 21H2 WIM file to VHDX file using Convert-WindowsImage.ps1 fork from https://github.com/nerdile/convert-windowsimage 
-$WimFile = "C:\WIM\REFW11-X64-21H2-Enterprise.wim"
+# Convert a Windows 11 WIM file to VHDX file using Convert-WindowsImage.ps1 fork from https://github.com/nerdile/convert-windowsimage 
+$WimFile = "C:\WIM\REFW11-X64-22H2-Enterprise.wim"
 $Edition = "Windows 11 Enterprise"
 $OutPutVHDXFile = "C:\VHDsV2\AP-W11-21H2.vhdx"
 
 # Create UEFI-based VHDX file
 C:\Setup\Scripts\Convert-WindowsImage.ps1 -SourcePath $WimFile -Edition $Edition -VHDPath $OutPutVHDXFile -VHDFormat VHDX -VHDType Dynamic -VHDPartitionStyle GPT -SizeBytes 240GB
-
-
-# Convert a Windows 11 22H2 WIM file to VHDX file using Convert-WindowsImage.ps1 fork from https://github.com/nerdile/convert-windowsimage 
-$WimFile = "C:\WIM\REFW11-X64-22H2-Enterprise.wim"
-$Edition = "Windows 11 Enterprise"
-$OutPutVHDXFile = "C:\VHDs\AP-W11-22H2.vhdx"
-
-# Create UEFI-based VHDX file
-C:\Setup\Scripts\Convert-WindowsImage.ps1 -SourcePath $WimFile -Edition $Edition -VHDPath $OutPutVHDXFile -VHDFormat VHDX -VHDType Dynamic -VHDPartitionStyle GPT -SizeBytes 240GB
-
