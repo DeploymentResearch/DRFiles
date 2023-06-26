64f7e489d0b56703930f876e06378aa775dc13b6
arwidmark <arwidmark@gmail.com>
2023-06-25 23:20:47 -0500

    Update SaveVMVideos.ps1

--- Scripts/Hyper-V/SaveVMVideos.ps1
+++ Scripts/Hyper-V/SaveVMVideos.ps1
@@ -1,7 +1,7 @@
 ﻿<#
 .Synopsis
     Sample script for Deployment Research
-    Script will generate videos of the screen from running Hyper-V VMs, useful for troubleshooting and documentation
+    Script will generate videos of the screen from Hyper-V VMs, useful for troubleshooting and documentation
 
 .DESCRIPTION
     Created: 2023-06-18
@@ -25,6 +25,13 @@
 #>
 
 $HyperVHosts = @(
+    "2PS-ROGUE-001"
+    "2PS-ROGUE-002"
+    "2PS-ROGUE-003"
+    "2PS-ROGUE-004"
+    "2PS-ROGUE-005"
+    "2PS-ROGUE-006"
+    "2PS-ROGUE-007"
     "2PS-ROGUE-010"
     "2PS-ROGUE-011"
     "2PS-ROGUE-012"
@@ -55,7 +62,7 @@ $LocalImagePath = "C:\temp\vmimages"
 # Folder for ffmpeg
 $ffmpegEXE = "E:\Setup\ffmpeg\ffmpeg.exe"
 
-# Amount of time to run image capture for each running VM on each Hyper-V Host
+# Amount of time to run image capture for each VM on each Hyper-V Host
 $RunTimeInSeconds = 2700
 
 # Amount of time between image capture
@@ -88,34 +95,47 @@ function WriteVMScreenShot  {
     )
 
 	$FinalImageName = "$ImagePath\$ImageName"
-    Write-Host $FinalImageName
+    #Write-Host $FinalImageName
 
     $VMCS = Get-WmiObject -ComputerName $HyperVHost -Namespace root\virtualization\v2 -Class Msvm_ComputerSystem -Filter "ElementName='$($VMName)'" 
 
-	# Get the resolution of the screen at the moment
-	$video = $VMCS.GetRelated("Msvm_VideoHead")
-	$xResolution = $video.CurrentHorizontalResolution[0]
-	$yResolution = $video.CurrentVerticalResolution[0]
+    If ($VMCS.OnTimeInMilliseconds -gt 0){
 
-	#(get-VMScreenShot $VMCS $xResolution $yResolution).Save($FinalImageName,$ImageType)
+	    #write-host "Entering VMCS section"
+        # Get the resolution of the screen at the moment
+	    $video = $VMCS.GetRelated("Msvm_VideoHead")
+	    $xResolution = $video.CurrentHorizontalResolution[0]
+	    $yResolution = $video.CurrentVerticalResolution[0]
 
-    # Get VM Object
-	$VMMS = Get-WmiObject -ComputerName $HyperVHost -Namespace root\virtualization\v2 -Class Msvm_VirtualSystemManagementService 
+        # Get VM Object
+	    $VMMS = Get-WmiObject -ComputerName $HyperVHost -Namespace root\virtualization\v2 -Class Msvm_VirtualSystemManagementService 
 
-    # Get screenshot
-    $image = $VMMS.GetVirtualSystemThumbnailImage($VMCS, $xResolution, $yResolution).ImageData
+        # Get screenshot
+        $image = $VMMS.GetVirtualSystemThumbnailImage($VMCS, $xResolution, $yResolution).ImageData
 
-    # Transform into bitmap
-    $BitMap = New-Object System.Drawing.Bitmap -Args $xResolution,$yResolution,Format16bppRgb565
-    $Rect = New-Object System.Drawing.Rectangle 0,0,$xResolution,$yResolution
-    $BmpData = $BitMap.LockBits($Rect,"ReadWrite","Format16bppRgb565")
-    [System.Runtime.InteropServices.Marshal]::Copy($Image, 0, $BmpData.Scan0, $BmpData.Stride*$BmpData.Height)
-    $BitMap.UnlockBits($BmpData)
+        # Transform into bitmap
+        $BitMap = New-Object System.Drawing.Bitmap -Args $xResolution,$yResolution,Format16bppRgb565
+        $Rect = New-Object System.Drawing.Rectangle 0,0,$xResolution,$yResolution
+        $BmpData = $BitMap.LockBits($Rect,"ReadWrite","Format16bppRgb565")
+        [System.Runtime.InteropServices.Marshal]::Copy($Image, 0, $BmpData.Scan0, $BmpData.Stride*$BmpData.Height)
+        $BitMap.UnlockBits($BmpData)
 
-    # Save to file
-    $BitMap.Save($FinalImageName,$ImageType)
-
-
+        # Save to file
+        $BitMap.Save($FinalImageName,$ImageType)
+    }
+    Else{
+        #write-host "Entering No VMCS section"
+        # VM is turned off, generate blank image to not break sequence number
+        $BitMap = New-Object System.Drawing.Bitmap -Args 1024,768,Format16bppRgb565
+        # $font = new-object System.Drawing.Font Consolas,16 
+        $brushBg = [System.Drawing.Brushes]::Black 
+        #$brushFg = [System.Drawing.Brushes]::White 
+        $graphics = [System.Drawing.Graphics]::FromImage($BitMap) 
+        $graphics.FillRectangle($brushBg,0,0,$BitMap.Width,$BitMap.Height) 
+        #$graphics.DrawString('VM is turned off',$font,$brushFg,10,10) 
+        $graphics.Dispose() 
+        $BitMap.Save($FinalImageName,$ImageType) 
+    }
 }
 
 # Build list of VMs on all Hyper-V Hosts
@@ -137,7 +157,7 @@ If ($Result){
     foreach ($Item in $Result) {
         Write-Warning "VM: $($Item.InputObject) is a duplicate" 
     }
-    Write-Warning "Duplicate VM Names exist, aborting..."
+    Write-Warning "Duplicate VM Names exist in VM list, aborting..."
     Break
 }
 
@@ -145,7 +165,7 @@ If ($Result){
 # Create local images folder on each Hyper-V Host (for better performance and higher framerates)
 Invoke-Command -Command { New-Item -Path $using:LocalImagePath -ItemType Directory -Force } -ComputerName $HyperVHosts
 
-# Start saving images from the running VMs
+# Start saving images from the VMs
 # Due to ffmpeg limitations in the Windows build, this script have to generate names in a sequence per VM
 $WriteVMScreenShotDefinition = "function WriteVMScreenShot { ${function:WriteVMScreenShot} }"
 foreach ($Server in $HyperVHosts) {
@@ -157,27 +177,20 @@ foreach ($Server in $HyperVHosts) {
         # Script Assemblies
         Add-Type -AssemblyName "System.Drawing"
 
-        # Get all running VMs
-        $VMs = Get-VM | Where-Object { $_.State -ne 'Off' }
-
-        If ($VMs){
-            # Running VMs found
-        }
-        Else {
-            # No running VMs found, aborting...
-            Break
-        }
-    
         $RunUntil = (Get-Date).AddSeconds($using:RunTimeInSeconds)
         $StartingSequenceNumber = 1
         While ($RunUntil -gt (Get-Date)) {
-            foreach ($VM in $VMs){
+            
+            # Get list of VMs for this server
+            $VMsPerServer = $using:VMInfo | Where-Object { $_.Server -eq $env:ComputerName }
+            foreach ($VM in $VMsPerServer){
                 $VMName = $VM.VMName
                 $ImageName = $VMName + "_" + ($StartingSequenceNumber | ForEach-Object{"{0:D5}" -f $_}) + ".$using:ImageType"
                 write-Host "About to run: writeVMScreenShot -ImagePath $using:LocalImagePath -ImageName "$ImageName" -ImageType $using:ImageType -HyperVHost Localhost -VMName $VMName"
                 writeVMScreenShot -ImagePath $using:LocalImagePath -ImageName "$ImageName" -ImageType $using:ImageType -HyperVHost Localhost -VMName  $VMName 
             }
-            $SleepTime = [Math]::Round(1000 / $CaptureFrameRate)
+            # Wait a bit for next capture according to CaptureFrameRate
+            $SleepTime = [Math]::Round(1000 / $using:CaptureFrameRate)
             Start-Sleep -Milliseconds $SleepTime
 
             # Increase sequence number
@@ -192,7 +205,7 @@ foreach ($Server in $HyperVHosts) {
 Write-Host "Entering wait cycle..."
 Start-Sleep -Seconds $RunTimeInSeconds
 
-break
+
 # Copy the still images from each Hyper-V host to this machine
 foreach ($Server in $HyperVHosts) {
     # Convert the local image path to a UNC path
@@ -205,7 +218,7 @@ foreach ($VM in $VMInfo){
     $VMName = $VM.VMName
     Set-Location $ImagePath
     If (Test-Path "$ImagePath\$($VMName)_00001.$imageType"){
-        Start-Process cmd.exe -ArgumentList "/c $ffmpegEXE -framerate $ResultingVideoFrameRate -i $ImagePath\$($VMName)_%05d.$imageType $VideoPath\$VMName.mp4 -y" -Wait
+        Start-Process $ffmpegEXE -ArgumentList "-framerate $ResultingVideoFrameRate -i $ImagePath\$($VMName)_%05d.$imageType $VideoPath\$VMName.mp4 -y" -Wait -WindowStyle Hidden
     }
     Else {
         Write-warning "No image sequence found for VM: $VMName"
@@ -221,5 +234,8 @@ Write-Host "Videos are saved in the  $VideoPath folder"
 # Optional cleanup of uploaded/copied images
 # If (test-Path $ImagePath) { Remove-item $ImagePath\* -Force }
 
+# Optional cleanup of generated videos
+# If (test-Path $VideoPath) { Remove-item $VideoPath\* -Force }
+
 # Optional cleanup of locally captured images
-# Invoke-Command -Command { Remove-Item -Path $using:LocalImagePath -Recurse -Force } -ComputerName $HyperVHosts
+# Invoke-Command -Command { Remove-Item -Path $using:LocalImagePath\* -Force } -ComputerName $HyperVHosts
