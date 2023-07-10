782b6e92e1eb2df304d71ab2b1f7a0af303b0b9a
arwidmark <arwidmark@gmail.com>
2023-07-10 00:36:40 -0500

    Create New-DriversPackageFromExport.ps1

---
+++ Scripts/Drivers/New-DriversPackageFromExport.ps1
@@ -0,0 +1,10 @@
+﻿$DriversPath = "C:\Drivers"
+$TempPath = "C:\Temp" 
+
+New-Item -Path $DriversPath -ItemType Directory -Force
+New-Item -Path $TempPath -ItemType Directory -Force
+
+Export-WindowsDriver -Online -Destination C:\Drivers
+
+New-WindowsImage -CapturePath $DriversPath -ImagePath "$TempPath\DriverPackage.wim" -Name "Driver Automation Tool Package"
+
