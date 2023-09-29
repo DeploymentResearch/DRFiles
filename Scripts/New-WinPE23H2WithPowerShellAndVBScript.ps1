86774f1caffdbc19a07e9af7f6543bd6d6b9d2f6
arwidmark <arwidmark@gmail.com>
2023-09-29 08:50:16 -0500

    Create New-WinPE23H2WithPowerShellAndVBScript.ps1

---
+++ Scripts/New-WinPE23H2WithPowerShellAndVBScript.ps1
@@ -0,0 +1,114 @@
+﻿<#
+.SYNOPSIS
+	Script to create a WinPE 25398 incuding VBScript Support
+	
+.DESCRIPTION
+    This script will inject downloaded FODs for VBScript while creating a WinPE media that also includes PowerShell
+
+.EXAMPLE
+	.\New-WinPE23H2WithPowerShellAndVBScript.ps1 
+
+.NOTES
+    FileName:    New-WinPE23H2WithPowerShellAndVBScript.ps1
+	Author:      Johan Arwidmark
+    Contact:     @jarwidmark
+    Created:     September 29, 2023
+    Updated:     September 29, 2023
+	
+    Version history:
+    1.0.0 - September 29, 2023 - Script created
+#>
+
+# Settings
+$WinPE_BuildFolder = "C:\Setup\WinPE11_23H2"
+$WinPE_Architecture = "amd64"
+$WinPE_MountFolder = "C:\Mount"
+$WinPE_ISOFolder = "C:\ISO"
+$WinPE_ISOfile = "C:\ISO\WinPE11_23H2_PowerShell_VBScript.iso"
+$VBSCRIPT_FOD = "C:\Setup\VBSCRIPT_FOD_25951"
+
+$ADK_Path = "C:\Program Files (x86)\Windows Kits\10\Assessment and Deployment Kit"
+$WinPE_ADK_Path = $ADK_Path + "\Windows Preinstallation Environment"
+$WinPE_OCs_Path = $WinPE_ADK_Path + "\$WinPE_Architecture\WinPE_OCs"
+$DISM_Path = $ADK_Path + "\Deployment Tools" + "\amd64\DISM"
+$OSCDIMG_Path = $ADK_Path + "\Deployment Tools" + "\amd64\Oscdimg"
+$OSCDIMG_arm64_Path = $ADK_Path + "\Deployment Tools" + "\$WinPE_Architecture\Oscdimg"
+
+# Validate locations
+If (!(Test-path $OSCDIMG_Path)){ Write-Warning "OSCDIMG Path does not exist, aborting...";Break}
+If (!(Test-path $VBSCRIPT_FOD)){ Write-Warning "VBSCRIPT_FOD Path does not exist, aborting...";Break}
+
+# Delete existing WinPE build folder (if exist)
+try 
+{
+if (Test-Path -path $WinPE_BuildFolder) {Remove-Item -Path $WinPE_BuildFolder -Recurse -ErrorAction Stop}
+}
+catch
+{
+    Write-Warning "Oupps, Error: $($_.Exception.Message)"
+    Write-Warning "Most common reason is existing WIM still mounted, use DISM /Cleanup-Wim to clean up and run script again"
+    Break
+}
+
+# Create Mount folder
+New-Item -Path $WinPE_MountFolder -ItemType Directory -Force
+
+# Create ISO folder
+New-Item -Path $WinPE_ISOFolder -ItemType Directory -Force
+
+# Make a copy of the WinPE boot image from Windows ADK
+if (!(Test-Path -path "$WinPE_BuildFolder\Sources")) {New-Item "$WinPE_BuildFolder\Sources" -Type Directory}
+Copy-Item "$WinPE_ADK_Path\$WinPE_Architecture\en-us\winpe.wim" "$WinPE_BuildFolder\Sources\boot.wim"
+
+# Copy WinPE boot files
+Copy-Item "$WinPE_ADK_Path\$WinPE_Architecture\Media\*" "$WinPE_BuildFolder" -Recurse
+ 
+# Mount the WinPE image
+$WimFile = "$WinPE_BuildFolder\Sources\boot.wim"
+Mount-WindowsImage -ImagePath $WimFile -Path $WinPE_MountFolder -Index 1
+ 
+# Add native WinPE optional components (using ADK version of dism.exe instead of Add-WindowsPackage)
+# Install WinPE-WMI before you install WinPE-NetFX (dependency)
+& $DISM_Path\dism.exe /Image:$WinPE_MountFolder /Add-Package /PackagePath:$WinPE_OCs_Path\WinPE-WMI.cab 
+& $DISM_Path\dism.exe /Image:$WinPE_MountFolder /Add-Package /PackagePath:$WinPE_OCs_Path\en-us\WinPE-WMI_en-us.cab
+& $DISM_Path\dism.exe /Image:$WinPE_MountFolder /Add-Package /PackagePath:$WinPE_OCs_Path\WinPE-NetFx.cab
+& $DISM_Path\dism.exe /Image:$WinPE_MountFolder /Add-Package /PackagePath:$WinPE_OCs_Path\en-us\WinPE-NetFx_en-us.cab
+& $DISM_Path\dism.exe /Image:$WinPE_MountFolder /Add-Package /PackagePath:$WinPE_OCs_Path\WinPE-PowerShell.cab
+& $DISM_Path\dism.exe /Image:$WinPE_MountFolder /Add-Package /PackagePath:$WinPE_OCs_Path\en-us\WinPE-PowerShell_en-us.cab
+& $DISM_Path\dism.exe /Image:$WinPE_MountFolder /Add-Package /PackagePath:$WinPE_OCs_Path\WinPE-DismCmdlets.cab
+& $DISM_Path\dism.exe /Image:$WinPE_MountFolder /Add-Package /PackagePath:$WinPE_OCs_Path\en-us\WinPE-DismCmdlets_en-us.cab
+
+# Add VBScript support from downloaded FOD
+& $DISM_Path\dism.exe /Image:$WinPE_MountFolder /Add-Package /PackagePath:$VBSCRIPT_FOD\Microsoft-Windows-VBSCRIPT-FoD-Package~31bf3856ad364e35~amd64~~.cab
+& $DISM_Path\dism.exe /Image:$WinPE_MountFolder /Add-Package /PackagePath:$VBSCRIPT_FOD\Microsoft-Windows-VBSCRIPT-FoD-Package~31bf3856ad364e35~amd64~en-us~.cab
+& $DISM_Path\dism.exe /Image:$WinPE_MountFolder /Add-Package /PackagePath:$VBSCRIPT_FOD\Microsoft-Windows-VBSCRIPT-FoD-Package~31bf3856ad364e35~wow64~~.cab
+& $DISM_Path\dism.exe /Image:$WinPE_MountFolder /Add-Package /PackagePath:$VBSCRIPT_FOD\Microsoft-Windows-VBSCRIPT-FoD-Package~31bf3856ad364e35~wow64~en-us~.cab
+
+# Write unattend.xml file to change screen resolution
+$UnattendPEx64 = @'
+<?xml version="1.0" encoding="utf-8"?>
+<unattend xmlns="urn:schemas-microsoft-com:unattend">
+    <settings pass="windowsPE">
+        <component name="Microsoft-Windows-Setup" processorArchitecture="arm64" publicKeyToken="31bf3856ad364e35" language="neutral" versionScope="nonSxS" xmlns:wcm="http://schemas.microsoft.com/WMIConfig/2002/State">
+            <Display>
+                <ColorDepth>32</ColorDepth>
+                <HorizontalResolution>1280</HorizontalResolution>
+                <RefreshRate>60</RefreshRate>
+                <VerticalResolution>720</VerticalResolution>
+            </Display>
+        </component>
+    </settings>
+</unattend>
+'@ | Out-File "$WinPE_MountFolder\Unattend.xml" -Encoding utf8 -Force
+
+# Unmount the WinPE image and save changes
+Dismount-WindowsImage -Path $WinPE_MountFolder -Save
+
+# Create a bootable WinPE ISO file using a single entry syntax (comment out if you don't need the ISO)
+$BootData='1#pEF,e,b"{0}"' -f "$OSCDIMG_arm64_Path\efisys_noprompt.bin"
+  
+$Proc = Start-Process -FilePath "$OSCDIMG_Path\oscdimg.exe" -ArgumentList @("-bootdata:$BootData",'-u2','-udfver102',"$WinPE_BuildFolder","$WinPE_ISOfile") -PassThru -Wait -NoNewWindow
+if($Proc.ExitCode -ne 0)
+{
+    Throw "Failed to generate ISO with exitcode: $($Proc.ExitCode)"
+}
