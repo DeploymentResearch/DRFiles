e609da0d2309693df1eb0b70a096037b407a2c23
arwidmark <arwidmark@gmail.com>
2023-12-18 06:41:36 -0600

    Create Get-MSIProductDetailsForInstalledSoftwareViaComObject.ps1

---
+++ Scripts/MSI/Get-MSIProductDetailsForInstalledSoftwareViaComObject.ps1
@@ -0,0 +1,10 @@
+﻿$Installer = New-Object -ComObject WindowsInstaller.Installer
+$InstallerProducts = $Installer.ProductsEx("", "", 7)
+$InstalledProducts = ForEach($Product in $InstallerProducts){
+    [PSCustomObject]@{ProductCode = $Product.ProductCode()
+    LocalPackage = $Product.InstallProperty("LocalPackage")
+    VersionString = $Product.InstallProperty("VersionString")
+    ProductPath = $Product.InstallProperty("ProductName")}
+} 
+
+$InstalledProducts | Where-Object { $_.productpath -like "*stifler*"  }
