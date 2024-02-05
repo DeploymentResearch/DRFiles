7d8bffb983e793e677c435fcac0a2a4becda3bf4
arwidmark <arwidmark@gmail.com>
2024-02-05 14:52:55 -0600

    Create UnpinInternetExplorer.ps1

---
+++ Scripts/Taskbar/UnpinInternetExplorer.ps1
@@ -0,0 +1,7 @@
+﻿# Unpin Internet Explorer from the Taskbar
+$AppName = @("Internet Explorer")
+$Action = "Unpin from taskbar"
+((New-Object -Com Shell.Application).NameSpace('shell:::{4234d49b-0245-4df3-b780-3893943456e1}').Items() | 
+  Where-Object { $AppName -eq $_.Name }).Verbs() | 
+  Where-Object {$_.Name.replace('&','') -match $Action} | 
+  ForEach-Object {$_.DoIt()}
