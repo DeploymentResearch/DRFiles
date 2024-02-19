b1d533ed524815765deec2fb8938e1923466905c
arwidmark <arwidmark@gmail.com>
2024-02-19 11:26:29 -0600

    Update Create-ConfigMgrSourceFolders.ps1

--- Scripts/Create-ConfigMgrSourceFolders.ps1
+++ Scripts/Create-ConfigMgrSourceFolders.ps1
@@ -9,20 +9,21 @@ If (-NOT ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdent
     Break
 }
 
-New-Item -Path E:\MigData -ItemType Directory
-New-Item -Path E:\Logs -ItemType Directory
-New-Item -Path E:\Sources -ItemType Directory
-New-Item -Path E:\Sources\OSD -ItemType Directory
-New-Item -Path E:\Sources\OSD\Boot -ItemType Directory
-New-Item -Path E:\Sources\OSD\DriverPackages -ItemType Directory
-New-Item -Path E:\Sources\OSD\DriverSources -ItemType Directory
-New-Item -Path E:\Sources\OSD\MDT -ItemType Directory
-New-Item -Path E:\Sources\OSD\OS -ItemType Directory
-New-Item -Path E:\Sources\OSD\Settings -ItemType Directory
-New-Item -Path E:\Sources\Software -ItemType Directory
-New-Item -Path E:\Sources\Software\Adobe -ItemType Directory
-New-Item -Path E:\Sources\Software\Microsoft -ItemType Directory
+New-Item -Path "E:\MigData" -ItemType Directory -Force
+New-Item -Path "E:\Logs" -ItemType Directory -Force
+New-Item -Path "E:\Setup" -ItemType Directory -Force
+New-Item -Path "E:\Sources" -ItemType Directory -Force
+New-Item -Path "E:\Sources\OSD" -ItemType Directory -Force
+New-Item -Path "E:\Sources\OSD\Boot" -ItemType Directory -Force
+New-Item -Path "E:\Sources\OSD\Branding" -ItemType Directory -Force
+New-Item -Path "E:\Sources\OSD\DriverPackages" -ItemType Directory -Force
+New-Item -Path "E:\Sources\OSD\DriverSources" -ItemType Directory -Force
+New-Item -Path "E:\Sources\OSD\OS" -ItemType Directory -Force
+New-Item -Path "E:\Sources\OSD\Settings" -ItemType Directory -Force
+New-Item -Path "E:\Sources\Software" -ItemType Directory -Force
+New-Item -Path "E:\Sources\Software\Adobe" -ItemType Directory -Force
+New-Item -Path "E:\Sources\Software\Microsoft" -ItemType Directory -Force
 
-net share 'Logs=E:\Logs' '/grant:EVERYONE,change'
+net share 'Logs$=E:\Logs' '/grant:EVERYONE,change'
 icacls E:\Logs /grant '"VIAMONSTRA\CM_NAA":(OI)(CI)(M)'
 net share 'Sources=E:\Sources' '/grant:EVERYONE,full'
