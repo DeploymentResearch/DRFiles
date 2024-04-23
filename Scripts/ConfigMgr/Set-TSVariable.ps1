15d18135bff28d447db2a114368a2657e4eaa4fb
arwidmark <arwidmark@gmail.com>
2024-04-23 14:48:18 -0500

    Samples

---
+++ Scripts/ConfigMgr/Set-TSVariable.ps1
@@ -0,0 +1,8 @@
+﻿$ComputerName = "TEST01"
+$MyCustomVariable = "WELL"
+
+$TSEnv = New-Object -ComObject Microsoft.SMS.TSEnvironment
+
+$TSEnv.Value("OSDComputerName") = $ComputerName
+$TSEnv.Value("MyCustomVariable") = $MyCustomVariable
+
