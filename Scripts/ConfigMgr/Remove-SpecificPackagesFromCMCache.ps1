098b30735ab9dd40d0bbd0ac8312d07952b82f28
arwidmark <arwidmark@gmail.com>
2024-07-19 15:56:47 -0500

    Create Remove-SpecificPackagesFromCMCache.ps1

---
+++ Scripts/ConfigMgr/Remove-SpecificPackagesFromCMCache.ps1
@@ -0,0 +1,14 @@
+﻿$resman = New-Object -ComObject "UIResource.UIResourceMgr"
+$cacheInfo = $resman.GetCacheInfo()
+
+# List all packages
+$cacheinfo.GetCacheElements()  
+
+# Delete specific pacakge
+$ContentID = "b74e8bb6-36ac-409c-8e9f-54127fe01ae0"
+$cacheinfo.GetCacheElements() | 
+    Where-Object {$_.ContentId -eq $ContentID } |
+    where-object {$_.LastReferenceTime -lt (get-date).AddDays(-7)} | 
+    foreach {
+        $cacheInfo.DeleteCacheElement($_.CacheElementID)
+    }
