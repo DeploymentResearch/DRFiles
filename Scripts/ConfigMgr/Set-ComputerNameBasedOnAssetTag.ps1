f0cc702ce9fef766b3c07bf7781eba4f99641b4c
arwidmark <arwidmark@gmail.com>
2024-10-01 00:38:57 -0500

    Create Set-ComputerNameBasedOnAssetTag.ps1

---
+++ Scripts/ConfigMgr/Set-ComputerNameBasedOnAssetTag.ps1
@@ -0,0 +1,9 @@
+﻿# Set ComputerName based on AssetTag
+$tsenv = New-Object -ComObject Microsoft.SMS.TSEnvironment
+$prefix = "ABC"
+$AssetTag = $tsenv.Value("AssetTag")
+
+If ($AssetTag){
+    $tsenv.Value("OSDComputerName") = ($prefix + $AssetTag).Substring(0,15)
+}
+
