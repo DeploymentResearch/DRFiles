5cac112b2fb49fa1319673137fc09ed9993a5968
arwidmark <arwidmark@gmail.com>
2024-11-11 14:34:51 -0600

    Create Get-PackageDetailsFromAllDPs.sql

---
+++ Scripts/ConfigMgr/Get-PackageDetailsFromAllDPs.sql
@@ -0,0 +1,18 @@
+SELECT p.Name, 
+	p.Description, 
+	n.SourceCompressedSize, 
+	n.SourceSize, 
+	dp.LastRefreshTime, 
+	p.Manufacturer, 
+	p.Version, 
+	p.Language, 
+	p.SourceSite, 
+	p.PackageID, 
+	dp.ServerNALPath as DP,
+	psd.InstallStatus
+FROM v_Package p
+INNER JOIN v_DistributionPoint dp ON p.PackageID = dp.PackageID
+LEFT JOIN v_PackageStatusRootSummarizer n ON p.PackageID = n.PackageID
+LEFT JOIN v_PackageStatusDistPointsSumm psd ON dp.ServerNALPath=psd.ServerNALPath
+AND dp.PackageID=psd.PackageID LEFT JOIN v_PackageStatus ps ON dp.ServerNALPath=ps.PkgServer
+AND dp.PackageID=ps.PackageID
