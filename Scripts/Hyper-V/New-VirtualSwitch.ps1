991a04afe128f418c5189df65a38d133f358f6ab
arwidmark <arwidmark@gmail.com>
2025-03-04 11:14:22 -0600

    Demo

---
+++ Scripts/Hyper-V/New-VirtualSwitch.ps1
@@ -0,0 +1,8 @@
+﻿# Create Internal Hyper-V Switch
+New-VMSwitch -Name Internal -SwitchType Internal | Out-Null
+
+# Create External Hyper-V Switch
+$NetworkAdapter = Get-NetAdapter | Where-Object Status -eq "Up" 
+New-VMSwitch -Name External -NetAdapterName ($NetworkAdapter.Name) -AllowManagementOs $true
+
+ 
