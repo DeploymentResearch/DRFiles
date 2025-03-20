dc20d02f4a13d43facd5d895ebba3b8340073cce
arwidmark <arwidmark@gmail.com>
2025-03-20 13:02:58 -0500

    Create SetOSDComputerName.ps1

---
+++ Scripts/ConfigMgr/SetOSDComputerName.ps1
@@ -0,0 +1,11 @@
+﻿$ComputerNamePrefix = "VOA"
+$SerialNumber = (Get-CimInstance -ClassName Win32_BIOS | Select-Object SerialNumber).SerialNumber
+# Validate length
+If ($SerialNumber.Length -gt 12){
+    # Use the first 12 characters
+    $SerialNumber = $SerialNumber.Substring(0,12)
+}
+
+$OSDComputerName = "VOA" + $SerialNumber
+$TSEnv = New-Object -COMObject Microsoft.SMS.TSEnvironment
+$TSEnv.Value("OSDComputerName") = $OSDComputerName
