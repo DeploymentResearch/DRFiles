9c9a89706b78c72c2ca56b7e1f4d6bcad5385858
arwidmark <arwidmark@gmail.com>
2025-05-22 10:14:30 -0500

    Update Uninstall-MicrosoftGraphModules.ps1

--- Scripts/Intune/Uninstall-MicrosoftGraphModules.ps1
+++ Scripts/Intune/Uninstall-MicrosoftGraphModules.ps1
@@ -1,14 +1,19 @@
 ﻿# After uninstalling, close any PowerShell sessions
-Uninstall-Module Microsoft.Graph -AllVersions
-Uninstall-Module Microsoft.Graph.Beta -AllVersions # Assuming you have beta modules installed.
 
+# Uninstall other Modules that may use it
 Uninstall-Module IntuneStuff -AllVersions
 Uninstall-Module WindowsAutoPilotIntune -AllVersions
 Uninstall-Module WindowsAutopilotIntuneCommunity -AllVersions
+Uninstall-Module IntuneDeviceInventory -AllVersions
 
+# Uninstall Microsoft Graph (first all modules except Authentication, then the Authentication module)
 Get-InstalledModule Microsoft.Graph.* | ForEach-Object { if($_.Name -ne "Microsoft.Graph.Authentication") { Uninstall-Module $_.Name -AllVersions } }
 Uninstall-Module Microsoft.Graph.Authentication -AllVersions
 
+# Close PowerShell sessions here....
 
 # Install the latest version
 Install-Module Microsoft.Graph -Verbose
+
+# Get number of Modules
+(Get-InstalledModule Microsoft.Graph.*).count
