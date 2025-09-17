168242fc129a2252b3fc8fa72ff29377669d9471
arwidmark <arwidmark@gmail.com>
2025-09-16 23:30:17 -0500

    Update CleanupBeforeW11Upgrade.ps1

--- Scripts/ConfigMgr/CleanupBeforeW11Upgrade.ps1
+++ Scripts/ConfigMgr/CleanupBeforeW11Upgrade.ps1
@@ -3,13 +3,13 @@
 # Twitter: @jarwidmark
 # LinkedIn: https://www.linkedin.com/in/jarwidmark
 
-$Logfile = "C:\Windows\Temp\CleanupBeforeW11Upgrade.log"
+$Logfile = "C:\Windows\Temp\Invoke-DiskCleanup.log"
 $StartTime = Get-Date
 
 # Script cleans the following components (change to $False to disable):
 $CleanDriversFolder = $True
 $CleanCBSTempFolder = $True
-$CleanConfigMgrCache = $True
+$CleanConfigMgrCache = $True # Configured for to delete content older than 5 days
 $CleanWindowsTempFolder = $True
 $CleanDOCache = $True
 $ClearBranchCacheCache = $True
@@ -51,13 +51,13 @@ Function FreeSpace {
 # Get Free Diskspace before cleanup
 $FreeSpaceBeforeCleanup = FreeSpace
 Write-Log "Cleanup Started"
-Write-Log "Free disk space before cleanup is $FreeSpaceBeforeCleanup"
+Write-Log "Free disk space before cleanup is $FreeSpaceBeforeCleanup GB"
 
 If ($CleanDriversFolder) {
     # Remove any existing drivers folder
     Write-Log "Removing any existing drivers folder"
     $CurrentDiskSpace = FreeSpace
-    Write-Log "- Free disk space before is: $CurrentDiskSpace"
+    Write-Log "- Free disk space before is: $CurrentDiskSpace GB"
     $Path = "C:\Drivers"
     If (Test-Path $Path){
         #Write-Log "Drivers folder found"
@@ -67,14 +67,14 @@ If ($CleanDriversFolder) {
         #Write-Log "Drivers folder not found"
     }
     $CurrentDiskSpace = FreeSpace
-    Write-Log "- Free disk space after is: $CurrentDiskSpace"
+    Write-Log "- Free disk space after is: $CurrentDiskSpace GB"
 }
 
 If ($CleanCBSTempFolder){
     # Remove content from CBSTemp
     Write-Log "Removing content from the CBSTemp folder"
     $CurrentDiskSpace = FreeSpace
-    Write-Log "- Free disk space before is: $CurrentDiskSpace"
+    Write-Log "- Free disk space before is: $CurrentDiskSpace GB"
     $Path = "C:\Windows\CbsTemp"
     If (Test-Path $Path){
         Remove-Item -Path $Path\* -Force -Recurse -ErrorAction SilentlyContinue
@@ -83,7 +83,7 @@ If ($CleanCBSTempFolder){
         Write-Log "CBSTemp folder not found"
     }
     $CurrentDiskSpace = FreeSpace
-    Write-Log "- Free disk space after is: $CurrentDiskSpace"
+    Write-Log "- Free disk space after is: $CurrentDiskSpace GB"
 }
 
 If ($CleanConfigMgrCache){
@@ -92,7 +92,7 @@ If ($CleanConfigMgrCache){
     # Including persisted cache items (DeleteCacheElementEx vs. DeleteCacheElement)
     Write-Log "Clearing ConfigMgr Cache"
     $CurrentDiskSpace = FreeSpace
-    Write-Log "- Free disk space before is: $CurrentDiskSpace"
+    Write-Log "- Free disk space before is: $CurrentDiskSpace GB"
     try {
 
         $MinDays = 5
@@ -107,19 +107,19 @@ If ($CleanConfigMgrCache){
         # No ConfigMgrClient
     }
     $CurrentDiskSpace = FreeSpace
-    Write-Log "- Free disk space after is: $CurrentDiskSpace"
+    Write-Log "- Free disk space after is: $CurrentDiskSpace GB"
 }
 
 If ($CleanWindowsTempFolder){
     # Remove files from C:\Windows\Temp older than 7 days
     Write-Log "Removing files from C:\Windows\Temp older than 7 days"
     $CurrentDiskSpace = FreeSpace
-    Write-Log "- Free disk space before is: $CurrentDiskSpace"
+    Write-Log "- Free disk space before is: $CurrentDiskSpace GB"
     $Path = "C:\Windows\Temp\*"
     $ContentToRemove = Get-ChildItem -Path $Path -Recurse -Force -ErrorAction SilentlyContinue | Where-Object { ($_.CreationTime -lt $(Get-Date).AddDays(-7)) } 
     $ContentToRemove | Remove-Item -Force -Recurse -ErrorAction SilentlyContinue
     $CurrentDiskSpace = FreeSpace
-    Write-Log "- Free disk space after is: $CurrentDiskSpace"
+    Write-Log "- Free disk space after is: $CurrentDiskSpace GB"
 }
 
 If ($CleanDOCache){
@@ -127,17 +127,34 @@ If ($CleanDOCache){
     # Clear Delivery Optimization Cache
     Write-Log "Clearing Delivery Optimization Cache"
     $CurrentDiskSpace = FreeSpace
-    Write-Log "- Free disk space before is: $CurrentDiskSpace"
-    Delete-DeliveryOptimizationCache -IncludePinnedFiles -Force
+    Write-Log "- Free disk space before is: $CurrentDiskSpace GB"
+    if (Get-Command Delete-DeliveryOptimizationCache -ErrorAction SilentlyContinue) {
+        #Delete-DeliveryOptimizationCache -IncludePinnedFiles -Force | Out-Null
+
+        # Run Delete-DeliveryOptimizationCache inside a job, to prevent output (the cmdlet ignores stream preferences)
+        $job = Start-Job -ScriptBlock {
+            $ProgressPreference='SilentlyContinue'
+            try { 
+                Delete-DeliveryOptimizationCache  -IncludePinnedFiles -Force -ErrorAction SilentlyContinue -WarningAction SilentlyContinue -InformationAction SilentlyContinue -Verbose:$false *> $null 
+            } catch {
+                # Do nothing
+            }
+        }
+        Wait-Job $job | Out-Null
+        Remove-Job $job -Force
+
+    } else {
+        Write-Log "Delete-DeliveryOptimizationCache cmdlet not available in this Windows version, skipping Delivery Optimization Cache cleanup"
+    }
     $CurrentDiskSpace = FreeSpace
-    Write-Log "- Free disk space after is: $CurrentDiskSpace"
+    Write-Log "- Free disk space after is: $CurrentDiskSpace GB"
 }
 
 If ($ClearBranchCacheCache){
     # Clear BranchCache Cache
     Write-Log "Clearing BranchCache Cache"
     $CurrentDiskSpace = FreeSpace
-    Write-Log "- Free disk space before is: $CurrentDiskSpace"
+    Write-Log "- Free disk space before is: $CurrentDiskSpace GB"
     Try{
         Clear-BCCache -Force -ErrorAction Stop
     }
@@ -145,21 +162,23 @@ If ($ClearBranchCacheCache){
         # Do Nothing
     }
     $CurrentDiskSpace = FreeSpace
-    Write-Log "- Free disk space after is: $CurrentDiskSpace"
+    Write-Log "- Free disk space after is: $CurrentDiskSpace GB"
 }
 
 If ($NativeDiskCleanup){
     # Run Disk Cleanup
     # when changing StateFlags number please check run command for cleanmgr
-    Write-Log "Clearing Disk using Native Disk Cleanup"
+    Write-Log "Clearing Disk using Disk Cleanup (CleanMgr.exe)"
+    Write-Log "Disk Cleanup is a built-in Windows utility that scans the system for unnesserary files"
+    Write-Log "Files, such as temporary files, old Windows Updates, setup log files, memory dumps and more"
     $CurrentDiskSpace = FreeSpace
-    Write-Log "- Free disk space before is: $CurrentDiskSpace"
+    Write-Log "- Free disk space before is: $CurrentDiskSpace GB"
     $SageSet = "StateFlags2024"
     $Base = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\VolumeCaches\"
 
     $Locations = @(
 	    "Active Setup Temp Folders"
-	    "BranchCache"
+	    # "BranchCache"
 	    "Content Index Cleaner"
 	    "D3D Shader Cache"
 	    "Delivery Optimization Files"
@@ -177,7 +196,7 @@ If ($NativeDiskCleanup){
 	    "Offline Pages Files"
 	    "Old ChkDsk Files"
 	    "Previous Installations"
-	    "Recycle Bin"
+	    # "Recycle Bin"
 	    "RetailDemo Offline Content"
 	    "Service Pack Cleanup"
 	    "Setup Log Files"
@@ -217,25 +236,34 @@ If ($NativeDiskCleanup){
     $cmdArgs = "/sagerun:$SageRunNumber"
     Write-Log "About to run CleanMgr"
     #Start-Process "$env:SystemRoot\System32\cleanmgr.exe" -ArgumentList $cmdArgs # -Wait
-    $CleanMgr = Start-Process "$env:SystemRoot\System32\cleanmgr.exe" -ArgumentList $cmdArgs -NoNewWindow -PassThru -RedirectStandardOutput $RedirectStandardOutput -RedirectStandardError $RedirectStandardError
+    $CleanMgr = Start-Process "$env:SystemRoot\System32\cleanmgr.exe" -ArgumentList $cmdArgs -WindowStyle Hidden -PassThru -RedirectStandardOutput $RedirectStandardOutput -RedirectStandardError $RedirectStandardError
 
-    Write-Log "CleanMgr Launched, now waiting for process to finish" 
-    $Processes = Get-Process -Name cleanmgr,dismhost -ErrorAction SilentlyContinue
-    If ($Processes) {
-        Write-Log "cleanmgr and dismhost processes are running"
+    Write-Log "CleanMgr Launched, now waiting for process to finish"
+    $TimeoutSeconds = 1200
+    $ElapsedSeconds = 0
+    $LogIntervalSeconds = 60
+
+    while ($ElapsedSeconds -lt $TimeoutSeconds) {
+        $Processes = Get-Process -Name cleanmgr,dismhost -ErrorAction SilentlyContinue
+        if ($Processes) {
+            $ElapsedMinutes = [math]::Floor($ElapsedSeconds / 60)
+            Write-Log "Waiting for cleanmgr/dismhost to finish... $ElapsedMinutes minutes elapsed"
+            Start-Sleep -Seconds $LogIntervalSeconds
+            $ElapsedSeconds += $LogIntervalSeconds
+        } else {
+            Write-Log "cleanmgr and dismhost processes have ended"
+            break
+        }
     }
-    Get-Process -Name cleanmgr,dismhost -ErrorAction SilentlyContinue | Wait-Process -Timeout 900
-    Write-Log "CleanMgr Ended"
 
     # CleanMgr error handling
     if ($CleanMgr.ExitCode -eq 0) {
 	    Write-Log "CleanMgr has been successfully processed"
     } elseif ($CleanMgr.ExitCode -gt 0) {
-	    return Write-Log "Exit code is $($CleanMgr.ExitCode)"
+	    return Write-Log "CleanMgr has been processed, exit code is $($CleanMgr.ExitCode)"
     } else {
-	    return Write-Log "An unknown error occurred."
+	    #return Write-Log "An unknown error occurred."
     }
-
 
     # Remove the Stateflags
     ForEach ($Location in $Locations)
@@ -243,15 +271,17 @@ If ($NativeDiskCleanup){
 	    Remove-ItemProperty -Path $($Base + $Location) -Name $SageSet -Force -ErrorAction SilentlyContinue | Out-Null
     }
     $CurrentDiskSpace = FreeSpace
-    Write-Log "- Free disk space after is: $CurrentDiskSpace"
+    Write-Log "- Free disk space after is: $CurrentDiskSpace GB"
 }
 
-# Get Free Diskspace after cleanup
+# Get Free Diskspace after native disk cleanup
 $FreeSpaceAfterCleanup = FreeSpace
 $DiskSpaceSavings = -([math]::Round($FreeSpaceBeforeCleanup - $FreeSpaceAfterCleanup,2))
-Write-Log "Cleanup Completed. The script cleaned $DiskSpaceSavings GB from $($Env:ComputerName)"
+Write-Log "Native disk Cleanup Completed. The script cleaned $DiskSpaceSavings GB from $($Env:ComputerName)"
 $EndTime = Get-Date
 $TimeSpan = New-TimeSpan -Start $StartTime -End $EndTime
 $DurationMinutes = $TimeSpan.Minutes
 $DurationSeconds = $TimeSpan.Seconds
 Write-Log "Script runtime was $DurationMinutes minutes and $DurationSeconds seconds"
+
+Write-OutPut "Cleanup complete - $DiskSpaceSavings GB freed"
