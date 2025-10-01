7aa365ea0c34e54107cde9c92b10929e4e0bc9a0
arwidmark <arwidmark@gmail.com>
2025-10-01 16:03:06 -0500

    Create Get-MDTTaskSequencesInConfigMgr.ps1

---
+++ Scripts/ConfigMgr/Get-MDTTaskSequencesInConfigMgr.ps1
@@ -0,0 +1,6 @@
+﻿# Search for MDT Templates
+$SearchString = "BDD"
+$Path = "E:\Demo\ExportedTaskSequences"
+$FilesWithMDTThings = Get-ChildItem -Path $Path -Filter *.XML -recurse | Select-String -pattern $SearchString | Group-Object path | Select-Object name
+
+$FilesWithMDTThings.count
