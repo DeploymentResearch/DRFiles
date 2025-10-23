d7e9d5b58be7eaf254b8a31a1ce3b9a0d4adeef7
arwidmark <arwidmark@gmail.com>
2025-10-23 11:23:48 -0500

    Update Export-TaskSequence.ps1

--- Scripts/ConfigMgr/Export-TaskSequence.ps1
+++ Scripts/ConfigMgr/Export-TaskSequence.ps1
@@ -6,8 +6,17 @@ Get-WmiObject SMS_TaskSequencePackage -Namespace root\sms\site_$SiteCode | Where
 # Export Task Sequences
 Set-Location E:\Demo\ExportedTaskSequences
 
-$TsList = Get-WmiObject SMS_TaskSequencePackage -Namespace root\sms\site_$SiteCode | Where-Object { $_.Name -eq "Windows 11 Enterprise x64 23H2 Native"}
+$TsList = Get-WmiObject SMS_TaskSequencePackage -Namespace root\sms\site_$SiteCode # | Where-Object { $_.Name -eq "Windows 11 Enterprise x64 23H2 Native MDM SQL"}
 ForEach ($Ts in $TsList){
     $Ts = [wmi]"$($Ts.__PATH)"
     Set-Content -Path "$($ts.PackageId).xml" -Value $Ts.Sequence
 }
+
+
+
+# Search for MDT Templates
+$SearchString = "BDD"
+$Path = "E:\Demo\ExportedTaskSequences"
+$FilesWithMDTThings = Get-ChildItem -Path $Path -Filter *.XML -recurse | Select-String -pattern $SearchString | Group-Object path | Select-Object name
+
+$FilesWithMDTThings.count
