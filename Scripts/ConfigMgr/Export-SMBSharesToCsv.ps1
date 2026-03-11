a2f12e58aa6fc79c4919cf3432e522a419db55cd
arwidmark <arwidmark@gmail.com>
2026-03-11 08:13:37 -0500

    Create Export-SMBSharesToCsv.ps1

---
+++ Scripts/ConfigMgr/Export-SMBSharesToCsv.ps1
@@ -0,0 +1,9 @@
+﻿$ExportPath = "C:\Temp8"
+$ExportFile = "$ExportPath\SmbShares.csv"
+
+If (!(Test-Path $ExportPath)){New-Item -Path $ExportPath -ItemType Directory -Force  }
+
+Get-SmbShare |
+    Where-Object Special -eq $false |
+    Select-Object Name, Path, Description, ScopeName, FolderEnumerationMode, CachingMode, ConcurrentUserLimit |
+    Export-Csv -Path $ExportFile -NoTypeInformation
