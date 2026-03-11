446afef4e4277fdfe792f732cdec283315bd2084
arwidmark <arwidmark@gmail.com>
2026-03-11 08:17:59 -0500

    Create Export-SMBSharePermissionsToCsv.ps1

---
+++ Scripts/ConfigMgr/Export-SMBSharePermissionsToCsv.ps1
@@ -0,0 +1,19 @@
+﻿$ExportPath = "C:\Temp"
+$ExportFile = "$ExportPath\SmbSharePermissions.csv"
+
+$shares = Get-SmbShare | Where-Object Special -eq $false
+
+$results = foreach ($share in $shares) {
+    foreach ($ace in Get-SmbShareAccess -Name $share.Name) {
+        [PSCustomObject]@{
+            ShareName     = $share.Name
+            Path          = $share.Path
+            Description   = $share.Description
+            AccountName   = $ace.AccountName
+            AccessControl = $ace.AccessControlType
+            AccessRight   = $ace.AccessRight
+        }
+    }
+}
+
+$results | Export-Csv -Path $ExportFile -NoTypeInformation
