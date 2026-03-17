8176949cf93ceb22579b3bf33a75ebf8d9ab2e6a
arwidmark <arwidmark@gmail.com>
2026-03-17 07:55:00 -0500

    Create Invoke-FullHardwareInventory.ps1

---
+++ Scripts/ConfigMgr/Invoke-FullHardwareInventory.ps1
@@ -0,0 +1,10 @@
+﻿$strAction = "{00000000-0000-0000-0000-000000000001}"
+
+Get-WmiObject -Namespace "root\ccm\invagt" -Class InventoryActionStatus | where {$_.InventoryActionID -eq "$strAction"} | Remove-WmiObject
+
+try {
+Invoke-WmiMethod -ComputerName $env:computername -Namespace root\ccm -Class SMS_Client -Name TriggerSchedule -ArgumentList $strAction -ErrorAction Stop | Out-Null
+}
+catch {
+write-host "$env:computername`: $_" -ForegroundColor Red
+}
