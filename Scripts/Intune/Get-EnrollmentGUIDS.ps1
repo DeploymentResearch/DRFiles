761e6328cc0df00e50c983ecce21c5dea270b7bc
arwidmark <arwidmark@gmail.com>
2026-04-14 09:29:54 -0500

    Create Get-EnrollmentGUIDS.ps1

---
+++ Scripts/Intune/Get-EnrollmentGUIDS.ps1
@@ -0,0 +1,10 @@
+﻿Get-ChildItem "HKLM:\SOFTWARE\Microsoft\Enrollments" |
+  ForEach-Object {
+    $props = Get-ItemProperty $_.PSPath
+    [PSCustomObject]@{
+      EnrollmentID = $_.PSChildName
+      UPN          = $props.UPN
+      MDMServer    = $props.ProviderID
+      EnrollType   = $props.EnrollmentType
+    }
+  } | Format-Table -AutoSize
