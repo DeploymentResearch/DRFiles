0adca540dc6ebd031bee818a662ee7e3f89d13aa
arwidmark <arwidmark@gmail.com>
2026-04-21 15:21:05 -0500

    Create Invoke-LicenseProcessing.ps1

---
+++ Scripts/Intune/Invoke-LicenseProcessing.ps1
@@ -0,0 +1,30 @@
+# Install the module. (You need admin on the machine.)
+# Install-Module Microsoft.Graph
+# Install-Module Microsoft.Graph.DeviceManagement
+Import-Module Microsoft.Graph.Authentication 
+$TenantID = "<tenant-id>"
+
+$Scopes = @(
+    "User.ReadWrite.All",
+    "Directory.ReadWrite.All"
+)
+
+# Connect to Graph
+Connect-MgGraph -TenantId $TenantID -Scopes $Scopes
+
+# Get all users 
+$Users = Get-MgUser -All
+
+# Get a single user
+$Users = Get-MgUser -Filter "displayName eq 'Johan'"
+
+# Invoke license processing for the user/users
+foreach ($User in $Users) {
+    try {
+        Invoke-MgLicenseUser -UserId $User.Id
+        Write-Host "Reprocessed: $($User.UserPrincipalName)" -ForegroundColor Green
+    }
+    catch {
+        Write-Warning "Failed for $($User.UserPrincipalName): $_"
+    }
+}
