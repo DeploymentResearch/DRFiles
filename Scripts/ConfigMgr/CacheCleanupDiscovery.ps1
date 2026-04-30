65e6a4c7565ddaf6a0e700ed1256de4e7aad53d8
arwidmark <arwidmark@gmail.com>
2026-04-30 11:14:03 -0500

    Cleanup

---
+++ Scripts/ConfigMgr/CacheCleanupDiscovery.ps1
@@ -0,0 +1,31 @@
+﻿# Get ConfigMgr Cache content older than 5 days
+try {
+
+    $MinDays = 5
+    $UIResourceMgr = New-Object -ComObject UIResource.UIResourceMgr -ErrorAction Stop
+    $Cache = $UIResourceMgr.GetCacheInfo()
+
+    $CacheElements = ($Cache.GetCacheElements() | Where-Object {[datetime]$_.LastReferenceTime -lt (Get-Date).adddays(-$MinDays)} | Measure-Object -Property CacheElementId).Count
+    
+}
+Catch{
+    # No ConfigMgr Client
+    Write-Host "No ConfigMgr Client"
+}
+
+
+If($CacheElements)
+{
+    If($CacheElements -eq 0)
+    {
+        Write-Host "Compliant"
+    }
+    Else
+    {
+        Write-Host "Non-compliant"
+    }
+}
+else
+{
+    Write-Host "Compliant"
+}
---
+++ Scripts/ConfigMgr/CacheCleanupRemediation.ps1
@@ -0,0 +1,17 @@
+﻿# Clear ConfigMgr Cache content older than 5 days
+# Including persisted cache items (DeleteCacheElementEx vs. DeleteCacheElement)
+try {
+
+    $MinDays = 5
+    $UIResourceMgr = New-Object -ComObject UIResource.UIResourceMgr -ErrorAction Stop
+    $Cache = $UIResourceMgr.GetCacheInfo()
+
+    $CacheElements = $Cache.GetCacheElements() | Where-Object {[datetime]$_.LastReferenceTime -lt (Get-Date).adddays(-$MinDays)}
+    foreach ($Element in $CacheElements) { $Cache.DeleteCacheElementEx($Element.CacheElementID, $true) }
+    
+}
+Catch{
+    # No ConfigMgr Client
+    Write-Host "No ConfigMgr Client"
+}
+
