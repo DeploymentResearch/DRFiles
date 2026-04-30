53cee2d133c531607736dc38b5b2ad9c68f9eeed
arwidmark <arwidmark@gmail.com>
2026-04-30 11:47:41 -0500

    Create Invoke-SecureBootUpdateDataInventoryBaseline.ps1

---
+++ Scripts/ConfigMgr/Invoke-SecureBootUpdateDataInventoryBaseline.ps1
@@ -0,0 +1,5 @@
+﻿$BaselineName = "CB - Secure Boot Update Data Inventory"
+$ComputerName = "Localhost"
+
+$Baselines = Get-WmiObject -ComputerName $ComputerName -Namespace root\ccm\dcm -Class SMS_DesiredConfiguration | Where-Object {$_.DisplayName -like $BaselineName}
+$Baselines | % { ([wmiclass]"\\$ComputerName\root\ccm\dcm:SMS_DesiredConfiguration").TriggerEvaluation($_.Name, $_.Version) }
