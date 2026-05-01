8394e36e674b37bee2186a16cd1d597280ccaa8c
arwidmark <arwidmark@gmail.com>
2026-05-01 15:51:17 -0500

    Create Get-CCMCacheEmptyFolders.ps1

---
+++ Scripts/ConfigMgr/Get-CCMCacheEmptyFolders.ps1
@@ -0,0 +1,5 @@
+﻿$CachePath = "C:\Windows\CCMCache"
+
+Get-ChildItem -Path $CachePath -Directory -Force | Where-Object {
+    -not (Get-ChildItem -Path $_.FullName -Force -Recurse -File -ErrorAction SilentlyContinue)
+} | Select-Object FullName, LastWriteTime
