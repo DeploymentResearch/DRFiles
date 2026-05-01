b36a82cd5bcc64a501053faa98671ae69d471429
arwidmark <arwidmark@gmail.com>
2026-05-01 15:57:36 -0500

    Update Get-CCMCacheEmptyFolders.ps1

--- Scripts/ConfigMgr/Get-CCMCacheEmptyFolders.ps1
+++ Scripts/ConfigMgr/Get-CCMCacheEmptyFolders.ps1
@@ -1,5 +1,12 @@
 ﻿$CachePath = "C:\Windows\CCMCache"
 
-Get-ChildItem -Path $CachePath -Directory -Force | Where-Object {
+$EmptyFolders = Get-ChildItem -Path $CachePath -Directory -Force | Where-Object {
     -not (Get-ChildItem -Path $_.FullName -Force -Recurse -File -ErrorAction SilentlyContinue)
 } | Select-Object FullName, LastWriteTime
+
+If ($EmptyFolders) {
+    Write-Host "Empty folder count is: $(($EmptyFolders | Measure-Object).Count)"
+}
+Else {
+    Write-Host "Empty folder count is: $(($EmptyFolders | Measure-Object).Count)"
+}
