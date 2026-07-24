ba26a933f2ffd4164b38e4e6121e27fba9acdb89
arwidmark <arwidmark@gmail.com>
2026-07-24 12:03:48 -0500

    Create Uninstall-CumulativeUpdates.ps1

---
+++ Scripts/ConfigMgr/Uninstall-CumulativeUpdates.ps1
@@ -0,0 +1,4 @@
+﻿Get-WindowsPackage -Online | Where-Object PackageName -like '*RollupFix*' |
+    Select-Object PackageName, PackageState, InstallTime
+
+Remove-WindowsPackage -Online -PackageName 'Package_for_RollupFix~31bf3856ad364e35~amd64~~26100.8875.1.28' -NoRestart
