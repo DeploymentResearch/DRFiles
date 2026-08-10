1512667e4692909e820ff7abaf8b8ce17d44f923
arwidmark <arwidmark@gmail.com>
2026-08-10 15:30:01 -0500

    SCCM

--- Scripts/ConfigMgr/Install-HYDDPPreReqsWS2022.ps1
+++ Scripts/ConfigMgr/Install-HYDDPPreReqsWS2022.ps1
@@ -1,8 +1,8 @@
-<#
+﻿<#
 
 ************************************************************************************************************************
 
-Created:	2025-11-25
+Created:	2026-08-09
 Version:	1.0
 
 Disclaimer:
@@ -20,33 +20,26 @@ Author - Johan Arwidmark
 # Requires Internet Access, if not, use -Source parameter
 
 Install-WindowsFeature `
-Web-Windows-Auth,
-Web-ISAPI-Ext,
-Web-Metabase,
-Web-WMI,
-BITS,
 RDC,
-NET-Framework-45-Features,
-NET-WCF-HTTP-Activation45,
-NET-WCF-TCP-PortSharing45,
-Web-Asp-Net45,
+Web-Server,
+Web-Common-Http,
 Web-Static-Content,
 Web-Default-Doc,
-Web-Dir-Browsing,
 Web-Http-Errors,
-Web-Http-Redirect,
 Web-App-Dev,
-Web-Net-Ext45,
-Web-ISAPI-Filter,
+Web-ISAPI-Ext,
 Web-Health,
 Web-Http-Logging,
 Web-Log-Libraries,
 Web-Request-Monitor,
-Web-Http-Tracing,
 Web-Security,
 Web-Filtering,
+Web-Windows-Auth,
 Web-Performance,
 Web-Stat-Compression,
+Web-Mgmt-Tools,
 Web-Mgmt-Console,
 Web-Scripting-Tools,
-Web-Mgmt-Compat
+Web-Mgmt-Compat,
+Web-Metabase,
+Web-WMI
