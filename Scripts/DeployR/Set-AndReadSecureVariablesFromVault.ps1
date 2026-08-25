b134026321eda2dd8ad999d736e25380d2bafa92
arwidmark <arwidmark@gmail.com>
2026-08-25 10:15:11 -0500

    Create Set-AndReadSecureVariablesFromVault.ps1

---
+++ Scripts/DeployR/Set-AndReadSecureVariablesFromVault.ps1
@@ -0,0 +1,13 @@
+﻿# On the DeployR Server
+# Prompting for a secret via Read-Host (Great)
+$password = Read-Host -Prompt 'Enter the admin password' -AsSecureString
+Set-Secret -Vault DeployR -Name AdminPassword -Secret $password -Verbose
+
+# In the TS (run on the client)
+
+try {
+    ${TSEnv:AdminPassword} = Get-Secret -Vault DeployR -Name "AdminPassword" | ConvertFrom-SecureString -AsPlainText -ErrorAction Stop
+}
+catch {
+    Write-Warning "AdminPassword secret not found. Checking task sequence environment variable fallback."
+}
