379100189247b4c879ea9f4ec2e27681bbd9b31d
arwidmark <arwidmark@gmail.com>
2026-08-25 11:41:06 -0500

    Create Invoke-BGinfo.ps1

---
+++ Scripts/DeployR/Invoke-BGinfo.ps1
@@ -0,0 +1,9 @@
+﻿# Show BGInfo during DeployR
+# Add x64 version of Bginfo.exe, and the BGI template to your boot image
+
+If (${TSEnv:ComputerName}){
+    $Null = New-Item -Path HKLM:\SOFTWARE\PSD -ItemType Directory -Force
+    $Null = New-ItemProperty -Path HKLM:\SOFTWARE\PSD -Name ComputerName -PropertyType String -Value ${TSEnv:ComputerName} -Force
+}
+
+& X:\Windows\System32\Bginfo.exe X:\Windows\System32\psd.bgi /timer:0 /NOLICPROMPT /SILENT
