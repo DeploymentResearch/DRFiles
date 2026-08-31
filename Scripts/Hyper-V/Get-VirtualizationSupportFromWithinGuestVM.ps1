83e4330f05bdbe439ab534e5c70862a40a78bf2c
arwidmark <arwidmark@gmail.com>
2026-08-31 15:51:04 -0500

    Create Get-VirtualizationSupportFromWithinGuestVM.ps1

---
+++ Scripts/Hyper-V/Get-VirtualizationSupportFromWithinGuestVM.ps1
@@ -0,0 +1,10 @@
+﻿$cs  = Get-CimInstance Win32_ComputerSystem
+$cpu = Get-CimInstance Win32_Processor | Select-Object -First 1
+
+[PSCustomObject]@{
+    Manufacturer      = $cs.Manufacturer
+    Model             = $cs.Model
+    HypervisorPresent = $cs.HypervisorPresent
+    NestedVirtExposed = $cpu.VMMonitorModeExtensions
+    SLAT              = $cpu.SecondLevelAddressTranslationExtensions
+}
